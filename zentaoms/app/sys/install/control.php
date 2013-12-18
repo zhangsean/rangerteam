4fb9fffd1ccd496b8cf0d9cac853e7dc3670d127
chunsheng <chunsheng@cnezsoft.com>
2013-12-18 00:53:35 +0000

    - remove the logic of setSite.
    git-svn-id: file:///tmp/ranzhi/trunk@17 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/install/control.php
+++ zentaoms/app/sys/install/control.php
@@ -21,7 +21,6 @@ class install extends control
     {
         if(!defined('RUN_MODE') or RUN_MODE != 'install') die('error');
         parent::__construct();
-        $this->loadModel('site')->setSite();
     }
 
     /**
