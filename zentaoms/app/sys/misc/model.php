a732629fdcd1ba8868c9e7ec942576843a767f56
wangyidong <wangyidong@cnezsoft.com>
2013-12-19 08:11:51 +0000

    + add empty model file.
    git-svn-id: file:///tmp/ranzhi/trunk@59 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ zentaoms/app/sys/misc/model.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * The model file of misc module of chanzhiEPS.
+ *
+ * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
+ * @license     LGPL (http://www.gnu.org/licenses/lgpl.html)
+ * @author      Yidong Wang <yidong@cnezsoft.com>
+ * @package     misc
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+class miscModel extends model
+{
+}
+
