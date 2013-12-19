864620d04e55efe9eeb8ac21659479fbdc575211
chunsheng <chunsheng@cnezsoft.com>
2013-12-19 05:33:39 +0000

    * remove empty lines.
    git-svn-id: file:///tmp/ranzhi/trunk@45 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/install/view/index.html.php
+++ zentaoms/app/sys/install/view/index.html.php
@@ -2,7 +2,6 @@
 /**
  * The html template file of index method of install module of chanzhiEPS.
  *
- *
  * @copyright   Copyright 2013-2013 青岛息壤网络信息有限公司 (QingDao XiRang Network Infomation Co,LTD www.xirangit.com)
  * @license     LGPL
  * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
