5a4cc7526b25442afed3881b10be654282c47785
zhujinyong <zhujinyong@cnezsoft.com>
2013-12-20 06:02:11 +0000

    * change chanzhiEPS to zentaoms.
    git-svn-id: file:///tmp/ranzhi/trunk@89 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/common/view/header.admin.html.php
+++ zentaoms/app/sys/common/view/header.admin.html.php
@@ -8,7 +8,7 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <?php echo html::a($this->createLink($this->config->default->module), $lang->chanzhiEPSx, "class='navbar-brand'");?>
+    <?php echo html::a($this->createLink($this->config->default->module), $lang->zentaoms, "class='navbar-brand'");?>
   </div>
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <?php echo commonModel::createMainMenu($this->moduleName);?>
