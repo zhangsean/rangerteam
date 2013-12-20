1d2df00d5d91196d5a2d3ac153d0df80470b50f1
zhujinyong <zhujinyong@cnezsoft.com>
2013-12-20 01:33:28 +0000

    * move common to ips.
    git-svn-id: file:///tmp/ranzhi/trunk@67 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/common/view/treeview.html.php
+++
@@ -1,3 +0,0 @@
-<?php css::import($themeRoot . 'default/treeview.css');?>
-<?php js::import($jsRoot . 'jquery/treeview/min.js');?>
-<script language='javascript'>$(function() {$(".tree").treeview({collapsed: false, unique: false}) })</script>
