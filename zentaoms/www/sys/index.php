a6e0cb4542c1f46cb76b07cb659331b6208c4af7
chunsheng <chunsheng@cnezsoft.com>
2013-12-20 02:37:21 +0000

    * move the loader logic to lodder.php.
    git-svn-id: file:///tmp/ranzhi/trunk@74 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/www/sys/index.php
+++ zentaoms/www/sys/index.php
@@ -1,35 +1,3 @@
 <?php
-/**
- * The sys app router file of ZenTaoMS.
- *
- * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
- * @license     商业软件，非开源软件
- * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
- * @package     ZenTaoMS
- * @version     $Id$
- * @link        http://www.zentao.net
- */
-/* Set the error reporting. */
-error_reporting(E_ALL);
-
-/* Start output buffer. */
-ob_start();
-
-/* Load the framework. */
-include '../../framework/router.class.php';
-include '../../framework/control.class.php';
-include '../../framework/model.class.php';
-include '../../framework/helper.class.php';
-
-/* Log the time and define the run mode. */
-$startTime = getTime();
-
-/* Run the app. */
-$app = router::createApp('sys');
-$common = $app->loadCommon();
-$app->parseRequest();
-$common->checkPriv();
-$app->loadModule();
-
-/* Flush the buffer. */
-echo helper::removeUTF8Bom(ob_get_clean());
+$appName = 'sys'; 
+include '../../framework/loader.php';
