c612e6a6ed63c0548b39cb5774635fb82e3f1033
zhujinyong <zhujinyong@cnezsoft.com>
2013-12-23 06:41:30 +0000

    - remove topbar and navs.
    git-svn-id: file:///tmp/ranzhi/trunk@121 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/common/view/header.html.php
+++ zentaoms/app/sys/common/view/header.html.php
@@ -16,7 +16,6 @@ js::set('lang', $lang->js);
 <div class='page-container'>
   <?php if(strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 6.0') !== false ) exit($lang->IE6Alert); ?>
   <header id='header' class='clearfix'>
-    <div id='headNav'><?php echo commonModel::printTopBar();?></div>
     <div id='headTitle'>
       <?php if(isset($config->site->logo)):?>
       <?php $logo = json_decode($config->site->logo);?>
@@ -29,46 +28,3 @@ js::set('lang', $lang->js);
       <div id='siteSlogan'><span><?php echo $this->config->site->slogan;?></span></div>
     </div>
   </header>
-
-  <?php $topNavs = $this->loadModel('nav')->getNavs('top');?>
-  <nav id='navbar' class='navbar navbar-default' role='navigation'>
-    <div class='navbar-header'>
-      <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#navbarCollapse'>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>
-      </button>
-      <a class='navbar-brand hidden-md hidden-lg' href='/'><i class='icon-home'></i></a>
-    </div>
-    <div class='collapse navbar-collapse' id='navbarCollapse'>
-      <!-- add css class '.nav-justified' to make the navbar fill the navbar. -->
-      <ul class='nav navbar-nav'>
-        <?php foreach($topNavs as $nav1):?>
-          <?php if(empty($nav1->children)):?>
-            <li class='<?php echo $nav1->class?>'><?php echo html::a($nav1->url, $nav1->title, !empty($nav1->target) ? "target='$nav1->target'" : '');?></li>
-            <?php else: ?>
-              <li class="dropdown <?php echo $nav1->class?>">
-                <?php echo html::a($nav1->url, $nav1->title . " <b class='caret'></b>", 'class="dropdown-toggle" data-toggle="dropdown" ' . (!empty($nav1->target) ? "target='$nav1->target'" : ''));?>
-                <ul class='dropdown-menu' role='menu'>
-                  <?php foreach($nav1->children as $nav2):?>
-                    <?php if(empty($nav2->children)):?>
-                      <li class='<?php echo $nav2->class?>'><?php echo html::a($nav2->url, $nav2->title, !empty($nav2->target) ? "target='$nav2->target'" : '');?></li>
-                    <?php else: ?>
-                      <li class='dropdown-submenu <?php echo $nav2->class?>'>
-                        <?php echo html::a($nav2->url, $nav2->title, !empty($nav2->target) ? "target='$nav2->target'" : '');?>
-                        <ul class='dropdown-menu' role='menu'>
-                          <?php foreach($nav2->children as $nav3):?>
-                          <li><?php echo html::a($nav3->url, $nav3->title, !empty($nav3->target) ? "target='$nav3->target'" : '');?></li>
-                          <?php endforeach;?>
-                        </ul>
-                      </li>
-                    <?php endif;?>
-                  <?php endforeach;?><!-- end nav2 -->
-                </ul>
-            </li>
-          <?php endif;?>
-        <?php endforeach;?><!-- end nav1 -->
-      </ul>
-    </div>
-  </nav>
-
