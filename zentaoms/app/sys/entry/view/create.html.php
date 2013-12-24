edf579424cc63ef20ebdbbc58ea86b80b6f644e4
zhujinyong <zhujinyong@cnezsoft.com>
2013-12-24 05:36:50 +0000

    * adjust style of entry.
    git-svn-id: file:///tmp/ranzhi/trunk@155 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/entry/view/create.html.php
+++ zentaoms/app/sys/entry/view/create.html.php
@@ -18,45 +18,47 @@ include '../../common/view/header.admin.html.php';
   <div class='panel-body'>
     <form method='post' class='form form-horizontal' id='ajaxForm'>
       <div class="form-group">
-        <label for='name' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->name;?></label>
+        <label for='name' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->name;?></label>
         <div class='col-md-4 col-sm-6'>
-          <?php 
-          echo html::input('name', '', "class='form-control' placeholder='{$lang->entry->note->name}'");
-          echo "<span><input type='checkbox' name='visible' value='1'>{$lang->entry->note->visible}</span>";
-          ?>
+          <?php echo html::input('name', '', "class='form-control' placeholder='{$lang->entry->note->name}'"); ?>
+        </div>
+        <div class="col-sm-6">
+          <span class="help-inline"><?php echo "<input type='checkbox' name='visible' value='1'>{$lang->entry->note->visible}";?></span>
         </div>
       </div>
       <div class="form-group">
-        <label for='code' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->code;?></label>
+        <label for='code' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->code;?></label>
         <div class='col-md-4 col-sm-6'>
           <?php echo html::input('code', '', "class='form-control' placeholder='{$lang->entry->note->code}'");?>
         </div>
       </div>
       <div class="form-group">
-        <label for='open' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->open;?></label>
+        <label for='open' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->open;?></label>
         <div class='col-md-4 col-sm-6'><?php echo html::select('open', $lang->entry->openList, '', 'class="form-control"');?></div>
       </div>
       <div class="form-group">
-        <label for='key' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->key;?></label>
+        <label for='key' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->key;?></label>
         <div class='col-md-4 col-sm-6'>
           <?php echo html::input('key', $key, "class='form-control' readonly='readonly'");?>
-          <?php echo html::a('javascript:void(0)', $lang->entry->createKey, 'onclick="createKey()"')?>
+        </div>
+        <div class="col-sm-6">
+          <span class="help-inline"><?php echo html::a('javascript:void(0)', $lang->entry->createKey, 'onclick="createKey()"')?></span>
         </div>
       </div>
       <div class="form-group">
-        <label for='login' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->login;?></label>
+        <label for='login' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->login;?></label>
         <div class='col-md-4 col-sm-6'><?php echo html::input('login', '', "class='form-control' placeholder='{$lang->entry->note->login}'");?></div>
       </div>
       <div class="form-group">
-        <label for='login' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->logout;?></label>
+        <label for='logout' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->logout;?></label>
         <div class='col-md-4 col-sm-6'><?php echo html::input('logout', '', "class='form-control' placeholder='{$lang->entry->note->logout}'");?></div>
       </div>
       <div class="form-group">
-        <label for='ip' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->ip;?></label>
+        <label for='ip' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->ip;?></label>
         <div class='col-md-4 col-sm-6'><?php echo html::input('ip', '', "class='form-control' placeholder='{$lang->entry->note->ip}'");?></div>
       </div>
       <div class="form-group">
-        <label for='ip' class='col-md-2 col-sm-3 control-label'><?php echo $lang->entry->logo;?></label>
+        <label for='ip' class='col-md-2 col-sm-3 control-label required'><?php echo $lang->entry->logo;?></label>
         <div class='col-md-4 col-sm-6'><input type='file' name='logo' id='logo' /></div>
       </div>
       <div class="form-group">
