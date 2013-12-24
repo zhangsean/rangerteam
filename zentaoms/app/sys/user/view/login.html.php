e4014458ba6f567127df422fd2ffa6af7683b575
wangyidong <wangyidong@cnezsoft.com>
2013-12-24 02:35:05 +0000

    * merge the page of login with admin and front.
    git-svn-id: file:///tmp/ranzhi/trunk@148 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/user/view/login.html.php
+++ zentaoms/app/sys/user/view/login.html.php
@@ -10,4 +10,29 @@
  * @link        http://www.zentao.net
  */
 ?>
-<?php RUN_MODE == 'front' ? include 'login.front.html.php' : include 'login.admin.html.php';?>
+<?php
+include '../../common/view/header.lite.html.php';
+js::import($jsRoot . 'md5.js');
+js::set('scriptName', $_SERVER['SCRIPT_NAME']);
+js::set('random', $this->session->random);
+css::internal('body{background-color:#f6f5f5}');
+?> 
+<style>body{padding-top:70px;}</style>
+<div class='container'>
+  <div id='adminLogin'>
+    <form method='post' id='ajaxForm'>
+      <div id='logo' class='text-center'><?php echo html::image("$themeRoot/default/images/main/logo.login.png");?></div>
+      <div id='responser' class='text-center'></div>
+      <?php echo html::input('account','',"class='form-control' placeholder='{$lang->user->inputAccountOrEmail}'");?>
+      <?php echo html::password('password','',"class='form-control' placeholder='{$lang->user->inputPassword}'");?>
+      <?php echo html::hidden('referer', $referer);?>
+      <?php echo html::submitButton($lang->user->login->common, 'btn btn-primary btn-block');?>
+    </form>
+  </div>
+</div>
+<?php
+if($config->debug) js::import($jsRoot . 'jquery/form/min.js');
+if(isset($pageJS)) js::execute($pageJS);
+?>
+</body>
+</html>
