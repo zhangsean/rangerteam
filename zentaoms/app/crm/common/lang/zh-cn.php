6f84d7f9ba20fe7565875cfbe7ed1c19d73b409b
guanxiying <guanxiying@cnezsoft.com>
2014-01-13 08:33:53 +0000

    * Adjust menu and layout frame.
    git-svn-id: file:///tmp/ranzhi/trunk@234 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/crm/common/lang/zh-cn.php
+++ zentaoms/app/crm/common/lang/zh-cn.php
@@ -11,3 +11,19 @@
  */
 $lang->app = new stdclass();
 $lang->app->name = 'CRM';
+
+$lang->menu->crm = new stdclass();
+$lang->menu->crm->dashboard = '我的地盘|dashboard|index|';
+$lang->menu->crm->order     = '订单|order|index|';
+$lang->menu->crm->task      = '任务|task|index|';
+$lang->menu->crm->customer  = '客户|customer|index|';
+$lang->menu->crm->contract  = '合同|contract|index|';
+$lang->menu->crm->product   = '产品|product|index|';
+$lang->menu->crm->contact   = '联系人|contact|index|';
+$lang->menu->crm->feedback  = '售后|feedback|index|';
+
+/* Menu of customer module. */
+$lang->customer = new stdclass();
+$lang->customer->menu = new stdclass();
+$lang->customer->menu->browse = array('link' => '客户列表|customer|admin|', 'alias' => 'edit');
+$lang->customer->menu->create = '添加客户|customer|create|type=customer';
--- zentaoms/app/sys/common/view/header.html.php
+++ zentaoms/app/sys/common/view/header.html.php
@@ -8,7 +8,7 @@
       <span class='icon-bar'></span>
       <span class='icon-bar'></span>
     </button>
-    <?php echo html::a($this->createLink($this->config->default->module), $lang->app->name, "class='navbar-brand'");?>
+    <?php echo html::a($this->createLink($this->config->default->module), RUN_MODE == 'front' ? $lang->app->name : $lang->zentaoms, "class='navbar-brand'");?>
   </div>
   <div class='collapse navbar-collapse navbar-ex1-collapse'>
     <?php echo commonModel::createMainMenu($this->moduleName);?>
