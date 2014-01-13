ef92031f15c950cdadbc9f3177ff5dfcdf3ead37
chunsheng <chunsheng@cnezsoft.com>
2014-01-13 06:27:39 +0000

    - remove this code.
    git-svn-id: file:///tmp/ranzhi/trunk@225 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/www/js/my.js
+++ zentaoms/www/js/my.js
@@ -10,12 +10,4 @@ $(document).ready(function()
 
     /* Ping for keep login every six minute. */
     if(needPing) setInterval('ping()', 1000 * 360);
-
-    $("#topNav li").hover(function () 
-    {
-        $(this).toggleClass('hover');
-    });
-
-    // active lightbox
-    $("[data-toggle=lightbox]").lightbox();
 });
