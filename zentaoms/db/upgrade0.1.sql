66b383cec9b140cf31e58d1e0371901b3b89bd07
wangyidong <wangyidong@cnezsoft.com>
2014-01-20 02:35:27 +0000

    * change for sql.
    git-svn-id: file:///tmp/ranzhi/trunk@343 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/db/upgrade0.1.sql
+++ zentaoms/db/upgrade0.1.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `sys_entry` CHANGE `login` `login` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `logo`,
 CHANGE `logout` `logout` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `login`,
-CHANGE `api` `blocks` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `logout`;
-ALTER TABLE `sys_entry` ADD `control` varchar(10) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'simple' AFTER `blocks`,
+CHANGE `api` `block` varchar(255) COLLATE 'utf8_general_ci' NOT NULL AFTER `logout`;
+ALTER TABLE `sys_entry` ADD `control` varchar(10) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'simple' AFTER `block`,
 ADD `size` varchar(50) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'max' AFTER `control`,
 ADD `position` varchar(10) COLLATE 'utf8_general_ci' NOT NULL DEFAULT 'default' AFTER `size`;
