2e469f1bd3f97cadbd3277113c17f74e90e01c76
guanxiying <guanxiying@cnezsoft.com>
2014-01-26 07:11:34 +0000

    * Fix task table name.
    git-svn-id: file:///tmp/ranzhi/trunk@428 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/config/config.php
+++ zentaoms/config/config.php
@@ -106,7 +106,7 @@ define('TABLE_WEBAPP',    'sys_webapp`');
 define('TABLE_LANG',      'sys_lang`');
 define('TABLE_ENTRY',     'sys_entry');
 define('TABLE_SSO',       'sys_sso');
-define('TABLE_TASK',      'sys_task`');
+define('TABLE_TASK',      'sys_task');
 
 /* Tables for crm. */
 define('TABLE_PRODUCT',     'sys_product');
