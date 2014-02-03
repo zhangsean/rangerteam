c31ab82aadee81d46297550eba7c60712e93681a
chunsheng <chunsheng@cnezsoft.com>
2014-02-03 09:29:45 +0000

    * init the menu of oa app.
    git-svn-id: file:///tmp/ranzhi/trunk@458 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ zentaoms/app/oa/common/lang/zh-cn.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The zh-cn file of common module of ZenTaoMS.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     商业软件，非开源软件
+ * @author      Chunsheng wang <chunsheng@cnezsoft.com>
+ * @package     common 
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+$lang->app = new stdclass();
+$lang->app->name = 'OA';
+
+$lang->menu->oa = new stdclass();
+$lang->menu->oa->dashboard = '我的地盘|my|index|';
+$lang->menu->oa->task      = '任务|task|index|';
+$lang->menu->oa->announce  = '公告|task|index|';
+$lang->menu->oa->leave     = '请假|leave|index|';
+$lang->menu->oa->claim     = '报销|claim|index|';
+$lang->menu->oa->checking  = '考勤|checking|index|';
+$lang->menu->oa->doc       = '文档|doc|index|';
+$lang->menu->oa->setting   = '设置|setting|index|';
--- zentaoms/app/oa/common/view/footer.html.php
+++ zentaoms/app/oa/common/view/footer.html.php
@@ -1,24 +1,8 @@
-  </div>
-  <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-  <div id='divider'></div>
-<?php $onlybody = zget($_GET, 'onlybody', 'no');?>
-<?php if($onlybody != 'yes'):?>
 </div>
-<div id='frontFooter'>
-  <div id="crumbs">
-    <?php commonModel::printBreadMenu($this->moduleName, isset($position) ? $position : ''); ?>
-  </div>
-  <div id="poweredby">
-    <span>Powered by <a href='http://www.zentao.net' target='_blank'>ZenTaoMS</a> (<?php echo $config->version;?>)</span>
-    <?php echo $lang->proVersion;?>
-    <?php commonModel::printNotifyLink();?>
-  </div>
-</div>
-<?php endif;?>
+<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <?php 
-js::set('onlybody', $onlybody);           // set the onlybody var.
 if($config->debug) js::import($jsRoot . 'jquery/form/min.js');
-if(isset($pageJS)) js::execute($pageJS);  // load the js for current page.
+if(isset($pageJS)) js::execute($pageJS);
 
 /* Load hook files for current page. */
 $extPath      = dirname(dirname(dirname(realpath($viewFile)))) . '/common/ext/view/';
