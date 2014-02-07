9ec03f7cadebefdb20aa9089323b92159946ae31
sunhao <sunhao@cnezsoft.com>
2014-02-07 03:31:06 +0000

    * use the shorter statement for char set.
    git-svn-id: file:///tmp/ranzhi/trunk@464 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/common/view/header.lite.html.php
+++ zentaoms/app/sys/common/view/header.lite.html.php
@@ -17,7 +17,7 @@ $themeRoot = $webRoot . "theme/";
 <!DOCTYPE html>
 <html>
 <head profile="http://www.w3.org/2005/10/profile">
-  <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <?php
