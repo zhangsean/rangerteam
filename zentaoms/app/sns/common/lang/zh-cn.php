d32d0525bc3ae3a8fc3d3603a95adc4ce3217c7f
chunsheng <chunsheng@cnezsoft.com>
2014-02-07 02:14:56 +0000

    * init the sns app menu.
    git-svn-id: file:///tmp/ranzhi/trunk@461 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ zentaoms/app/sns/common/lang/zh-cn.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The zh-cn file of common module of ZenTaoMS.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     商业软件，非开源软件
+ * @author      Chunsheng wang <chunsheng@cnezsoft.com>
+ * @package     common 
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+$lang->app = new stdclass();
+$lang->app->name = 'SNS';
+
+$lang->menu->sns = new stdclass();
+$lang->menu->sns->dashboard = '我的地盘|dashboard|index|';
+$lang->menu->sns->weibo     = '微博|weibo|index|';
+$lang->menu->sns->blog      = '博客|blog|index|';
+$lang->menu->sns->forum     = '论坛|forum|index|';
+$lang->menu->sns->event     = '活动|event|index|';
+$lang->menu->sns->food      = '订餐|food|index|';
+$lang->menu->sns->share     = '分享|share|index|';
+$lang->menu->sns->message   = '消息|message|index|';
+$lang->menu->sns->workmate  = '通讯录|workmate|index|';
+$lang->menu->sns->album     = '相册|album|index|';
--- zentaoms/app/sns/common/view/footer.html.php
+++ zentaoms/app/sns/common/view/footer.html.php
@@ -1,24 +1,8 @@
-  </div>
-  <?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
-  <iframe frameborder='0' name='hiddenwin' id='hiddenwin' scrolling='no' class='<?php print($config->debug ? 'debugwin' : 'hidden');?>'></iframe>
-  <div id='divider'></div>
-<?php $onlybody = zget($_GET, 'onlybody', 'no');?>
-<?php if($onlybody != 'yes'):?>
 </div>
-<div id='frontFooter'>
-  <div id="crumbs">
-    <?php commonModel::printBreadMenu($this->moduleName, isset($position) ? $position : ''); ?>
-  </div>
-  <div id="poweredby">
-    <span>Powered by <a href='http://www.zentao.net' target='_blank'>ZenTaoMS</a> (<?php echo $config->version;?>)</span>
-    <?php echo $lang->proVersion;?>
-    <?php commonModel::printNotifyLink();?>
-  </div>
-</div>
-<?php endif;?>
+<?php if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}?>
 <?php 
-js::set('onlybody', $onlybody);           // set the onlybody var.
-if(isset($pageJS)) js::execute($pageJS);  // load the js for current page.
+if($config->debug) js::import($jsRoot . 'jquery/form/min.js');
+if(isset($pageJS)) js::execute($pageJS);
 
 /* Load hook files for current page. */
 $extPath      = dirname(dirname(dirname(realpath($viewFile)))) . '/common/ext/view/';
