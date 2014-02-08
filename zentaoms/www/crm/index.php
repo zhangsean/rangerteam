9911ff284fb10dd0e30adcc809a74d9a7b7fa351
guanxiying <guanxiying@cnezsoft.com>
2014-02-08 06:34:59 +0000

    * Add comments of router file.
    git-svn-id: file:///tmp/ranzhi/trunk@474 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/www/crm/index.php
+++ zentaoms/www/crm/index.php
@@ -1,3 +1,13 @@
 <?php
+/**
+ * The crm app router file of ZenTaoMS.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     商业软件，非开源软件
+ * @author      Chunsheng Wang <chunsheng@cnezsoft.com>
+ * @package     ZenTaoMS
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
 $appName = 'crm'; 
 include '../../framework/loader.php';
