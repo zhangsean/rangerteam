d68458be43a2b5fa8906b7b866425d9f6305fe31
wangyidong <wangyidong@cnezsoft.com>
2014-02-18 08:21:25 +0000

    - remove useless code.
    git-svn-id: file:///tmp/ranzhi/trunk@561 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- zentaoms/app/sys/block/lang/zh-cn.php
+++ zentaoms/app/sys/block/lang/zh-cn.php
@@ -1,9 +1,6 @@
 <?php
 $lang->block->common = '区块';
 $lang->block->name   = '区块名称';
-$lang->block->set    = '区块设置';
-$lang->block->custom = '配置区块';
-$lang->block->add    = '添加区块';
 
 $lang->block->lblEntry = '应用';
 $lang->block->lblBlock = '区块';
