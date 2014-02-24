5e49882ff3cd3fa16789ca82c050aab5b1aa7d22
guanxiying <guanxiying@cnezsoft.com>
2014-02-24 08:17:52 +0000

    * Add common modal header and footer.
    git-svn-id: file:///tmp/ranzhi/trunk@621 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ zentaoms/app/crm/common/view/header.modal.html.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The common modal header view file of ZenTaoMS.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     商业软件，非开源软件
+ * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @package     ZenTaoMS
+ * @version     $Id$
+ * @link        http://www.zentao.net
+ */
+?>
+<div class="modal-dialog" style="width:700px;">
+  <div class="modal-content">
+    <div class="modal-header">
+      <?php echo html::closeButton();?>
+      <h4 class="modal-title"><?php echo $title; ?></h4>
+    </div>
