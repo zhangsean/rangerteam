f8923ab7972f1b6625b1995c9b26be47535ec937
wangyidong <wangyidong@cnezsoft.com>
2014-04-03 05:50:09 +0000

    * add note for doc config.
    git-svn-id: file:///tmp/ranzhi/trunk@659 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/oa/doc/config.php
+++ ranzhi/app/oa/doc/config.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * The config file of doc module of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Yidong Wang <yidong@cnezsoft.com>
+ * @package     doc 
+ * @version     $Id $
+ * @link        http://www.zentao.net
+ */
 global $lang;
 
 $config->doc = new stdclass();
