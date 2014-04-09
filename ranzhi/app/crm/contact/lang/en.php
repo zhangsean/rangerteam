ab9cce9a3d7673134ef16e3821099b815aee66c2
daitingting <daitingting@cnezsoft.com>
2014-04-09 08:40:34 +0000

    * Add en file of contact.
    git-svn-id: file:///tmp/ranzhi/trunk@699 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/crm/contact/lang/en.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The contact module en file of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     contact
+ * @version     $Id $
+ * @link        http://www.ranzhi.org
+ */
+$lang->contact->common        = 'Contact';
+$lang->contact->id            = 'ID';
+$lang->contact->customer      = 'Customer';
+$lang->contact->realname      = 'Name';
+$lang->contact->nickname      = 'Nick';
+$lang->contact->avatar        = 'Avatar';
+$lang->contact->birthday      = 'Birthday';
+$lang->contact->gender        = 'Gender';
+$lang->contact->email         = 'Email';
+$lang->contact->skype         = 'Skype';
+$lang->contact->qq            = 'QQ';
+$lang->contact->yahoo         = 'Y!';
+$lang->contact->gtalk         = 'GTalk';
+$lang->contact->wangwang      = 'Wangwang';
+$lang->contact->site          = 'Site';
+$lang->contact->mobile        = 'Mobile';
+$lang->contact->phone         = 'Phone';
+$lang->contact->weibo         = 'Weibo';
+$lang->contact->weixin        = 'Wechat';
+$lang->contact->desc          = 'Desc';
+$lang->contact->createdBy     = 'Created By';
+$lang->contact->createdDate   = 'Created Date';
+$lang->contact->editedBy      = 'Edited By';
+$lang->contact->editedDate    = 'Edited Date';
+$lang->contact->contactedBy   = 'Contacted By';
+$lang->contact->contactedDate = 'Contacted Date';
+$lang->contact->uploadAvatar  = 'Upload';
+
+$lang->contact->genderList['f'] = 'Female';
+$lang->contact->genderList['m'] = 'Male';
+
+$lang->contact->create = 'Create';
+$lang->contact->list   = 'List';
+$lang->contact->edit   = 'Update';
+
+$lang->contact->basicInfo   = 'Basic Info';
+$lang->contact->contactInfo = 'Contact Info';
