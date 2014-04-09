d85b99870f82aa376f62d3ab37561d8bbddf0d95
daitingting <daitingting@cnezsoft.com>
2014-04-09 08:58:29 +0000

    * Add en file of contract module.
    git-svn-id: file:///tmp/ranzhi/trunk@700 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/crm/contract/lang/en.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The en file of crm contract module of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     contract 
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->contract->common = 'Contract';
+
+$lang->contract->id           = 'ID';
+$lang->contract->order        = 'Order';
+$lang->contract->customer     = 'Customer';
+$lang->contract->name         = 'Name';
+$lang->contract->code         = 'Code';
+$lang->contract->amount       = 'Amount';
+$lang->contract->items        = 'Items';
+$lang->contract->begin        = 'Start Time';
+$lang->contract->end          = 'End Time';
+$lang->contract->delivery     = 'Delivery';
+$lang->contract->return       = 'Return';
+$lang->contract->status       = 'Status';
+$lang->contract->contact      = 'Contact';
+$lang->contract->signedBy     = 'Signed By';
+$lang->contract->signedDate   = 'Signed Date';
+$lang->contract->finishedBy   = 'Finished By';
+$lang->contract->finishedDate = 'Finished Date';
+$lang->contract->canceledBy   = 'Canceled By';
+$lang->contract->canceledDate = 'Canceled Date';
+$lang->contract->createdBy    = 'Created By';
+$lang->contract->createdDate  = 'Created Date';
+$lang->contract->editedBy     = 'Edited By';
+$lang->contract->editedDate   = 'Edited Date';
+
+$lang->contract->list    = 'List';
+$lang->contract->create  = 'Create';
+$lang->contract->edit    = 'Update';
+$lang->contract->setting = 'Setting';
+
+$lang->contract->deliveryList[]        = '';
+$lang->contract->deliveryList['wait']  = 'Wait';
+$lang->contract->deliveryList['doing'] = 'Doing';
+$lang->contract->deliveryList['done']  = 'Done';
+
+$lang->contract->returnList[]        = '';
+$lang->contract->returnList['wait']  = 'Wait';
+$lang->contract->returnList['doing'] = 'Doing';
+$lang->contract->returnList['done']  = 'Done';
+
+$lang->contract->statusList[]           = '';
+$lang->contract->statusList['normal']   = 'Normal';
+$lang->contract->statusList['closed']   = 'Closed';
+$lang->contract->statusList['canceled'] = 'Canceled';
+
+$lang->contract->codeUnitList[]        = '';
+$lang->contract->codeUnitList['Y']     = 'Year';
+$lang->contract->codeUnitList['m']     = 'Month';
+$lang->contract->codeUnitList['d']     = 'Day';
+$lang->contract->codeUnitList['fix']   = 'Fix';
+$lang->contract->codeUnitList['input'] = 'Input';
+
+$lang->contract->info = 'Info';
