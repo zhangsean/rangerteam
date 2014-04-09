96c16ece31f5d554289ceebe9f8bb7c3817578b9
guanxiying <guanxiying@cnezsoft.com>
2014-04-09 06:51:16 +0000

    * add en lang file.
    git-svn-id: file:///tmp/ranzhi/trunk@694 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/crm/block/lang/en.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The en file of block module of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @package     block 
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->block->order    = 'My Orders';
+$lang->block->task     = 'My Tasks';
+$lang->block->contract = 'My Contracts';
+
+$lang->block->lblBlock = 'Block';
+
+$lang->block->num     = 'Amount';
+$lang->block->orderBy = 'Sort NO.';
+$lang->block->status  = 'Status';
+$lang->block->asc     = 'ASC';
+$lang->block->desc    = 'DESC';
+$lang->block->actions = 'Options';
+
+$lang->block->admin   = 'Manage Blocks';
