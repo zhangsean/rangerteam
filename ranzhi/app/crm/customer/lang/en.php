a255dace20a003b3c65c69bae47df7ad12b2dbbb
daitingting <daitingting@cnezsoft.com>
2014-04-09 09:21:26 +0000

    * Add en file of customer.
    git-svn-id: file:///tmp/ranzhi/trunk@701 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/crm/customer/lang/en.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The customer module en file of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     customer
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->customer->common      = 'Customer';
+$lang->customer->id          = 'ID';
+$lang->customer->name        = 'Name';
+$lang->customer->type        = 'Type';
+$lang->customer->size        = 'Size';
+$lang->customer->industry    = 'Industry';
+$lang->customer->area        = 'Area';
+$lang->customer->status      = 'Status';
+$lang->customer->level       = 'Level';
+$lang->customer->site        = 'Site';
+$lang->customer->weibo       = 'Weibo';
+$lang->customer->weixin      = 'Wechat';
+$lang->customer->referType   = 'Refer Type';
+$lang->customer->desc        = 'Desc';
+$lang->customer->createdBy   = 'Created By';
+$lang->customer->createdDate = 'Created Date';
+$lang->customer->editedBy    = 'Edited By';
+$lang->customer->editedDate  = 'Edited Date';
+$lang->customer->contactBy   = 'Contact By';
+$lang->customer->contactDate = 'Contact Date';
+$lang->customer->nextDate    = 'Next Date';
+
+$lang->customer->typeList['national']   = 'National';
+$lang->customer->typeList['collective'] = 'Collective';
+$lang->customer->typeList['corporate']  = 'Corporate';
+$lang->customer->typeList['limited']    = 'Limited';
+$lang->customer->typeList['privated']   = 'Privated';
+$lang->customer->typeList['foreign']    = 'Foreign';
+$lang->customer->typeList['personal']   = 'Personal';
+
+$lang->customer->statusList['potential'] = 'Potential';
+$lang->customer->statusList['intension'] = 'Intension';
+$lang->customer->statusList['payed']     = 'Payed';
+$lang->customer->statusList['failed']    = 'Failed';
+
+$lang->customer->sizeList[1] = 'Large-scale';
+$lang->customer->sizeList[2] = 'Medium-scale';
+$lang->customer->sizeList[3] = 'Small-scale';
+$lang->customer->sizeList[4] = 'Mini-scale';
+
+$lang->customer->create = 'Create';
+$lang->customer->list   = 'List';
+$lang->customer->edit   = 'Update';
+
+$lang->customer->basicInfo = 'Basic Info';
+$lang->customer->moreInfo  = 'More Info';
