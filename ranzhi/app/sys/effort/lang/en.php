3f2d053566341d10fd51b4a998801f264b56665f
daitingting <daitingting@cnezsoft.com>
2014-04-10 03:08:29 +0000

    * Add en file of effort module.
    git-svn-id: file:///tmp/ranzhi/trunk@712 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/sys/effort/lang/en.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * The effort module english file of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     effort
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->effort->common          = 'Effort';
+$lang->effort->index           = "Index";
+$lang->effort->create          = "Create";
+$lang->effort->batchCreate     = "Create";
+$lang->effort->createForObject = "Create for object";
+$lang->effort->edit            = "Edit";
+$lang->effort->batchEdit       = "Batck edit";
+$lang->effort->view            = "Info";
+$lang->effort->viewAB          = "Info";
+$lang->effort->comment         = 'Comment';
+$lang->effort->export          = "Export";
+$lang->effort->delete          = "Delete";
+$lang->effort->browse          = "Browse";
+$lang->effort->history         = "History";
+$lang->effort->hour            = "Hour";
+
+$lang->effort->id          = 'ID';
+$lang->effort->account     = 'Owner';
+$lang->effort->date        = 'Date';
+$lang->effort->left        = 'Left';
+$lang->effort->consumed    = 'Consumed';
+$lang->effort->objectType  = 'objectType';
+$lang->effort->objectID    = 'objectID';
+$lang->effort->work        = 'Work';
+$lang->effort->deal        = 'deal with';
+
+$lang->effort->week         = '(l)';  // date function's param.
+$lang->effort->today        = 'Today';
+$lang->effort->weekDateList = '';
+
+$lang->effort->objectTypeList['custom']   = '';
+$lang->effort->objectTypeList['order']    = 'Order';
+$lang->effort->objectTypeList['contract'] = 'Contract';
+$lang->effort->objectTypeList['task']     = 'Task';
+
+$lang->effort->todayEfforts     = 'Today';
+$lang->effort->yesterdayEfforts = 'Yesterday';
+$lang->effort->thisWeekEfforts  = 'Thisweek';
+$lang->effort->lastWeekEfforts  = 'Lastweek';
+$lang->effort->thisMonthEfforts = 'Thismonth';
+$lang->effort->lastMonthEfforts = 'Lastmonth';
+$lang->effort->allDaysEfforts   = 'All';
+
+$lang->effort->noEmpty    = 'Must be not empty;';
+$lang->effort->isNumber   = 'Must be number;';
+$lang->effort->tooLang    = 'Effort content of ID%s is too lang.';
+$lang->effort->nowork     = "Work content of ID%s must be not empty！";
+$lang->effort->notice     = '(notice:1.if the %s is empty, it is invalid;2.if the %s is not equal %s and %s is empty, it is invalid)';
