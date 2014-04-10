11a9bc1a8560e57154a458e77269d4adc3675c88
daitingting <daitingting@cnezsoft.com>
2014-04-10 07:39:13 +0000

    * Add en file of common module.
    git-svn-id: file:///tmp/ranzhi/trunk@726 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/cash/common/lang/en.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The en file of common module of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     common 
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->app = new stdclass();
+$lang->app->name = 'CASH';
+
+$lang->menu->cash = new stdclass();
+$lang->menu->cash->dashboard = 'Dashboard|index|index|';
+$lang->menu->cash->trade     = 'Trade|trade|index|';
+$lang->menu->cash->invocie   = 'Invocie|invocie|index|';
+$lang->menu->cash->claim     = 'Claim|claim|index|';
+$lang->menu->cash->report    = 'Report|report|index|';
+$lang->menu->cash->account   = 'Account|account|index|';
+$lang->menu->cash->setting   = 'Setting|setting|index|';
