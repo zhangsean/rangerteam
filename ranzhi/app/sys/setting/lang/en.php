6a1e9a65c58dfe197692f794c92ba8f4c6020cae
daitingting <daitingting@cnezsoft.com>
2014-04-10 03:29:32 +0000

    * Add en file of setting module.
    git-svn-id: file:///tmp/ranzhi/trunk@713 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/sys/setting/lang/en.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The setting module english file of Ranzhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Yidong Wang <yidong@cnezsoft.com>
+ * @package     setting 
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->setting->common = 'Setting';
+$lang->setting->reset  = 'Reset';
+$lang->setting->key    = 'Key';
+$lang->setting->value  = 'Value';
+
+$lang->setting->module = new stdClass();
+$lang->setting->module->user     = 'User role';
+$lang->setting->module->product  = 'Product status';
+$lang->setting->module->customer = 'Customer level';
+
+$lang->setting->user = new stdClass();
+$lang->setting->user->fields['roleList'] = 'role list';
+
+$lang->setting->product = new stdClass();
+$lang->setting->product->fields['statusList'] = 'product status';
+
+$lang->setting->customer = new stdClass();
+$lang->setting->customer->fields['typeList'] = 'customer level';
+
+$lang->setting->currentLang = 'For current language';
+$lang->setting->allLang     = 'For all language';
