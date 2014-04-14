e5a9c71770962069e7efa2604adb683342f0f00a
chunsheng <chunsheng@cnezsoft.com>
2014-04-14 06:16:38 +0000

    * extract table.
    git-svn-id: file:///tmp/ranzhi/trunk@758 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/db/ranzhi.sql
+++ ranzhi/db/ranzhi.sql
@@ -1,23 +1,3 @@
--- DROP TABLE IF EXISTS `cash_invocie`;
-CREATE TABLE `cash_invocie` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `customer` smallint(5) unsigned NOT NULL,
-  `contract` mediumint(9) NOT NULL,
-  `order` mediumint(9) NOT NULL,
-  `contact` mediumint(9) NOT NULL,
-  `title` char(100) NOT NULL,
-  `content` char(100) NOT NULL,
-  `type` char(10) NOT NULL,
-  `amount` float(12,2) NOT NULL,
-  `point` float(4,2) NOT NULL,
-  `operator` char(30) NOT NULL,
-  `time` datetime NOT NULL,
-  `express` char(30) NOT NULL,
-  `ticket` char(30) NOT NULL,
-  KEY `customer` (`customer`),
-  KEY `contract` (`contract`),
-  KEY `order` (`order`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `crm_address`;
 CREATE TABLE `crm_address` (
   `id` mediumint(8) unsigned NOT NULL,
@@ -174,55 +154,6 @@ CREATE TABLE `crm_order` (
   KEY `contactedBy` (`contactedBy`),
   KEY `contactedDate` (`contactedDate`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `crm_orderAction`;
-CREATE TABLE `crm_orderAction` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `product` smallint(5) unsigned NOT NULL,
-  `action` char(30) NOT NULL,
-  `name` char(100) NOT NULL,
-  `conditions` text NOT NULL,
-  `inputs` text NOT NULL,
-  `results` text NOT NULL,
-  `tasks` text NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `product` (`product`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `crm_orderField`;
-CREATE TABLE `crm_orderField` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `product` mediumint(8) unsigned NOT NULL,
-  `field` varchar(50) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `order` tinyint(3) NOT NULL,
-  `control` varchar(10) NOT NULL,
-  `options` varchar(255) NOT NULL,
-  `default` varchar(100) NOT NULL,
-  `rules` varchar(255) NOT NULL,
-  `placeholder` varchar(100) NOT NULL,
-  `desc` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `product` (`product`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `crm_plan`;
-CREATE TABLE `crm_plan` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `customer` mediumint(8) unsigned NOT NULL,
-  `contract` mediumint(8) unsigned NOT NULL,
-  `type` char(30) NOT NULL,
-  `batch` tinyint(3) unsigned NOT NULL,
-  `amount` float(12,2) NOT NULL,
-  `deadline` date NOT NULL,
-  `status` char(30) NOT NULL,
-  `assignedTo` char(30) NOT NULL,
-  `assignedBy` char(30) NOT NULL,
-  `assignedDate` datetime NOT NULL,
-  `createdBy` char(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `finishedBy` char(30) NOT NULL,
-  `finishedDate` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `customer` (`customer`,`contract`,`type`,`deadline`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `crm_relation`;
 CREATE TABLE `crm_relation` (
   `contact` mediumint(8) unsigned NOT NULL,
@@ -235,54 +166,12 @@ CREATE TABLE `crm_relation` (
   KEY `contact` (`contact`),
   KEY `customer` (`customer`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `crm_request`;
-CREATE TABLE `crm_request` (
-  `id` mediumint(8) unsigned NOT NULL,
-  `customer` mediumint(8) NOT NULL,
-  `product` mediumint(8) unsigned NOT NULL,
-  `contact` char(30) NOT NULL,
-  `category` mediumint(8) unsigned NOT NULL,
-  `pri` tinyint(3) unsigned DEFAULT NULL,
-  `title` char(150) NOT NULL,
-  `desc` text NOT NULL,
-  `status` char(30) NOT NULL,
-  `createdBy` char(30) NOT NULL,
-  `createdDate` datetime NOT NULL,
-  `assignedTo` char(30) NOT NULL,
-  `assignedBy` char(30) NOT NULL,
-  `assignedDate` datetime NOT NULL,
-  `viewedBy` char(30) NOT NULL,
-  `viewedDate` datetime NOT NULL,
-  `repliedBy` char(30) NOT NULL,
-  `repliedDate` datetime NOT NULL,
-  `transferedBy` char(30) NOT NULL,
-  `transferedDate` datetime NOT NULL,
-  `editedBy` char(30) NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `closedBy` char(30) NOT NULL,
-  `closedReason` char(30) NOT NULL,
-  `closedDate` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `customer` (`customer`),
-  KEY `product` (`product`),
-  KEY `category` (`category`),
-  KEY `pri` (`pri`),
-  KEY `status` (`status`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `crm_service`;
 CREATE TABLE `crm_service` (
   `customer` smallint(8) NOT NULL,
   `product` mediumint(8) NOT NULL,
   `expire` date NOT NULL,
   UNIQUE KEY `customer` (`customer`,`product`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `crm_team`;
-CREATE TABLE `crm_team` (
-  `order` mediumint(8) unsigned NOT NULL,
-  `account` char(30) NOT NULL,
-  `role` char(30) NOT NULL,
-  `join` date NOT NULL,
-  PRIMARY KEY (`order`,`account`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `oa_article`;
 CREATE TABLE `oa_article` (
@@ -316,29 +205,6 @@ CREATE TABLE `oa_block` (
   `title` varchar(60) NOT NULL,
   `content` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `oa_book`;
-CREATE TABLE `oa_book` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(150) NOT NULL,
-  `alias` varchar(100) NOT NULL,
-  `keywords` varchar(150) NOT NULL,
-  `summary` text NOT NULL,
-  `content` text NOT NULL,
-  `type` enum('book','chapter','article') NOT NULL,
-  `parent` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `path` char(255) NOT NULL DEFAULT '',
-  `grade` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `author` varchar(60) NOT NULL,
-  `editor` varchar(60) NOT NULL,
-  `addedDate` datetime NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `views` mediumint(5) unsigned NOT NULL DEFAULT '0',
-  `order` smallint(5) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `order` (`order`),
-  KEY `parent` (`parent`),
-  KEY `path` (`path`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `oa_doc`;
 CREATE TABLE `oa_doc` (
@@ -383,87 +249,12 @@ CREATE TABLE `oa_effort` (
   `end` smallint(4) unsigned zerofill NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `oa_layout`;
-CREATE TABLE `oa_layout` (
-  `page` varchar(30) NOT NULL,
-  `region` varchar(30) NOT NULL,
-  `blocks` varchar(255) NOT NULL,
-  UNIQUE KEY `layout` (`page`,`region`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `oa_relation`;
 CREATE TABLE `oa_relation` (
   `type` char(20) NOT NULL,
   `id` mediumint(9) NOT NULL,
   `category` smallint(5) NOT NULL,
   UNIQUE KEY `relation` (`type`,`id`,`category`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sns_message`;
-CREATE TABLE `sns_message` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `type` char(20) NOT NULL,
-  `objectType` varchar(30) NOT NULL DEFAULT '',
-  `objectID` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `from` char(30) NOT NULL,
-  `to` char(30) NOT NULL,
-  `phone` char(30) NOT NULL,
-  `email` varchar(90) NOT NULL,
-  `qq` char(30) NOT NULL,
-  `date` datetime NOT NULL,
-  `content` text NOT NULL,
-  `link` varchar(100) NOT NULL,
-  `ip` varchar(15) NOT NULL,
-  `status` char(20) NOT NULL,
-  `public` enum('0','1') NOT NULL DEFAULT '1',
-  `readed` enum('0','1') NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `status` (`status`),
-  KEY `object` (`objectType`,`objectID`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sns_reply`;
-CREATE TABLE `sns_reply` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `thread` mediumint(8) unsigned NOT NULL,
-  `content` text NOT NULL,
-  `author` char(30) NOT NULL,
-  `editor` char(30) NOT NULL,
-  `addedDate` datetime NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `hidden` enum('0','1') NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `thread` (`thread`),
-  KEY `author` (`author`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sns_tag`;
-CREATE TABLE `sns_tag` (
-  `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `tag` varchar(50) NOT NULL,
-  `link` varchar(100) NOT NULL,
-  `rank` smallint(5) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `tag` (`tag`),
-  KEY `rank` (`rank`),
-  KEY `link` (`link`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sns_thread`;
-CREATE TABLE `sns_thread` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `board` mediumint(9) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `author` varchar(60) NOT NULL,
-  `editor` varchar(60) NOT NULL,
-  `addedDate` datetime NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `readonly` tinyint(1) NOT NULL DEFAULT '0',
-  `views` smallint(5) unsigned NOT NULL DEFAULT '0',
-  `stick` enum('0','1','2','3') NOT NULL DEFAULT '0',
-  `replies` smallint(6) NOT NULL,
-  `repliedBy` varchar(30) NOT NULL,
-  `repliedDate` datetime NOT NULL,
-  `replyID` mediumint(8) unsigned NOT NULL,
-  `hidden` enum('0','1') NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `category` (`board`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `sys_action`;
 CREATE TABLE `sys_action` (
@@ -557,21 +348,6 @@ CREATE TABLE `sys_file` (
   PRIMARY KEY (`id`),
   KEY `object` (`objectType`,`objectID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sys_group`;
-CREATE TABLE `sys_group` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `name` char(30) NOT NULL,
-  `role` char(30) NOT NULL DEFAULT '',
-  `desc` char(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sys_groupPriv`;
-CREATE TABLE `sys_groupPriv` (
-  `group` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `module` char(30) NOT NULL DEFAULT '',
-  `method` char(30) NOT NULL DEFAULT '',
-  UNIQUE KEY `group` (`group`,`module`,`method`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `sys_history`;
 CREATE TABLE `sys_history` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
@@ -580,37 +356,6 @@ CREATE TABLE `sys_history` (
   `old` text NOT NULL,
   `new` text NOT NULL,
   `diff` mediumtext NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sys_issue`;
-CREATE TABLE `sys_issue` (
-  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `product` mediumint(8) unsigned NOT NULL,
-  `category` mediumint(8) unsigned NOT NULL,
-  `customer` mediumint(8) NOT NULL,
-  `contact` mediumint(8) NOT NULL,
-  `pri` tinyint(3) unsigned DEFAULT NULL,
-  `title` varchar(255) NOT NULL,
-  `desc` text NOT NULL,
-  `type` varchar(30) NOT NULL,
-  `addedBy` char(30) NOT NULL,
-  `addedDate` datetime NOT NULL,
-  `viewedDate` datetime NOT NULL,
-  `assignedTo` char(30) NOT NULL,
-  `assignedBy` char(30) NOT NULL,
-  `assignedDate` datetime NOT NULL,
-  `repliedBy` char(30) NOT NULL,
-  `repliedDate` datetime NOT NULL,
-  `transferedBy` char(30) NOT NULL,
-  `transferedDate` datetime NOT NULL,
-  `editedBy` char(30) NOT NULL,
-  `editedDate` datetime NOT NULL,
-  `closedBy` char(30) NOT NULL,
-  `closedDate` datetime NOT NULL,
-  `closedReason` varchar(30) NOT NULL,
-  `toObjectType` varchar(30) NOT NULL,
-  `toObjectID` mediumint(8) unsigned NOT NULL,
-  `status` varchar(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 -- DROP TABLE IF EXISTS `sys_lang`;
@@ -725,13 +470,3 @@ CREATE TABLE `sys_user` (
   KEY `account` (`account`,`password`),
   KEY `dept` (`dept`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
--- DROP TABLE IF EXISTS `sys_userGroup`;
-CREATE TABLE `sys_userGroup` (
-  `account` char(30) NOT NULL DEFAULT '',
-  `group` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  UNIQUE KEY `account` (`account`,`group`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO `sys_entry` (`id`, `name`, `code`, `open`, `key`, `ip`, `logo`, `login`, `logout`, `block`, `control`, `size`, `position`, `visible`, `order`) VALUES
-(2, 'crm', 'crm', 'iframe', 'epet8b8ae1g89rxzquf4ubv37ul5tite', '*', '/theme/default/images/ips/app-crm.png', '../crm/', '', '', 'simple', 'max', 'default', 1, 0),
-(3, 'oa', 'oa', 'iframe', '1a673c4c3c85fadcf0333e0a4596d220', '*', '/theme/default/images/ips/app-oa.png', '../oa/', '', '', 'simple', 'max', 'default', 1, 1);
