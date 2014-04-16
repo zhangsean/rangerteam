7ee2ff14449b2f0c5953e4fa0b822d2e5e3b7d5c
daitingting <daitingting@cnezsoft.com>
2014-04-16 09:10:55 +0000

    * Add fields in crm_contract table.
    git-svn-id: file:///tmp/ranzhi/trunk@797 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/db/upgrade0.5.sql
+++ ranzhi/db/upgrade0.5.sql
@@ -2,3 +2,9 @@ ALTER TABLE `crm_customer` ADD `intension` text NOT NULL AFTER `level`;
 ALTER TABLE `sys_product` CHANGE `summary`  `desc` text NOT NULL;
 ALTER TABLE `crm_customer` DROP `referType`;
 ALTER TABLE `crm_customer` DROP `referID`;
+
+ALTER TABLE `crm_contract`
+ADD `deliveredBy` char(30) COLLATE 'utf8_general_ci' NOT NULL AFTER `signedDate`,
+ADD `deliveredDate` datetime NOT NULL AFTER `deliveredBy`,
+ADD `returnedBy` char(30) COLLATE 'utf8_general_ci' NOT NULL AFTER `deliveredDate`,
+ADD `returnedDate` datetime NOT NULL AFTER `returnedBy`;
