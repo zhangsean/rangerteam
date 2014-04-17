7588ed2e8d9d923fcb15deadbd2ff9002d7f129d
guanxiying <guanxiying@cnezsoft.com>
2014-04-17 05:12:54 +0000

    * rm unused codes.
    git-svn-id: file:///tmp/ranzhi/trunk@805 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/product/config.php
+++ ranzhi/app/crm/product/config.php
@@ -14,36 +14,3 @@ $config->product->require->create = 'name, code';
 $config->product->require->edit   = 'name, code';
 
 $config->product->action = new stdclass();
-
-global $lang;
-$config->product->action->common = new stdclass();
-$config->product->action->common->createOrder = new stdclass();
-$config->product->action->common->createOrder->condations = array();
-$config->product->action->common->createOrder->inputs     = array();
-$orderInputs = array('customer' => array('rules' => 'require'));
-
-$config->field = new stdclass();
-$config->field->require = new stdclass();
-$config->field->require->create = 'name, field, product, control';
-$config->field->require->edit   = 'name, field, control';
-
-$config->field->controlTypeList = array();
-$config->field->controlTypeList['input']    = 'varchar(200)';
-$config->field->controlTypeList['textarea'] = 'text';
-$config->field->controlTypeList['date']     = 'date';
-$config->field->controlTypeList['datetime'] = 'datetime';
-$config->field->controlTypeList['select']   = 'varchar(200)';
-$config->field->controlTypeList['radio']    = 'varchar(200)';
-$config->field->controlTypeList['checkbox'] = 'varchar(200)';
-$config->field->controlTypeList['user']     = 'varchar(200)';
-$config->field->controlTypeList['contact']  = 'varchar(200)';
-$config->field->controlTypeList['customer'] = 'varchar(200)';
-
-$config->field->lengthList[10]  = ',date,';
-$config->field->lengthList[19]  = ',datetime,';
-$config->field->lengthList[200] = ',input,select,radio,checkbox,user,contact,customer,';
-
-$config->action = new stdclass();
-$config->action->require = new stdclass();
-$config->action->require->create = 'name, action, product';
-$config->action->require->edit   = 'name, action';
