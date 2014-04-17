ca42a4903d8e27aa70f3ede446f7ecf8162d8862
daitingting <daitingting@cnezsoft.com>
2014-04-17 02:38:46 +0000

    * Adjust the order.
    git-svn-id: file:///tmp/ranzhi/trunk@801 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/customer/view/browse.html.php
+++ ranzhi/app/crm/customer/view/browse.html.php
@@ -20,15 +20,15 @@
     <thead>
       <tr class='text-center'>
         <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
-        <th class='w-60px'> <?php commonModel::printOrderLink('id',          $orderBy, $vars, $lang->customer->id);?></th>
-        <th>                <?php commonModel::printOrderLink('name',        $orderBy, $vars, $lang->customer->name);?></th>
-        <th class='w-150px'><?php commonModel::printOrderLink('createdDate', $orderBy, $vars, $lang->customer->createdDate);?></th>
-        <th class='w-60px'><?php commonModel::printOrderLink('type',        $orderBy, $vars, $lang->customer->type);?></th>
-        <th class='w-60px'> <?php commonModel::printOrderLink('status',      $orderBy, $vars, $lang->customer->status);?></th>
-        <th class='w-60px'> <?php commonModel::printOrderLink('size',        $orderBy, $vars, $lang->customer->size);?></th>
-        <th class='w-100px'> <?php commonModel::printOrderLink('level',       $orderBy, $vars, $lang->customer->level);?></th>
-        <th class='w-150px'> <?php commonModel::printOrderLink('contactDate', $orderBy, $vars, $lang->customer->contactDate);?></th>
-        <th class='w-100px'> <?php commonModel::printOrderLink('nextDate',       $orderBy, $vars, $lang->customer->nextDate);?></th>
+        <th class='w-60px'> <?php commonModel::printOrderLink('id',           $orderBy, $vars, $lang->customer->id);?></th>
+        <th>                <?php commonModel::printOrderLink('name',         $orderBy, $vars, $lang->customer->name);?></th>
+        <th class='w-60px'> <?php commonModel::printOrderLink('level',       $orderBy, $vars, $lang->customer->level);?></th>
+        <th class='w-60px'> <?php commonModel::printOrderLink('status',       $orderBy, $vars, $lang->customer->status);?></th>
+        <th class='w-60px'> <?php commonModel::printOrderLink('size',         $orderBy, $vars, $lang->customer->size);?></th>
+        <th class='w-60px'><?php commonModel::printOrderLink('type',          $orderBy, $vars, $lang->customer->type);?></th>
+        <th class='w-100px'> <?php commonModel::printOrderLink('contactDate', $orderBy, $vars, $lang->customer->contactDate);?></th>
+        <th class='w-100px'> <?php commonModel::printOrderLink('nextDate',    $orderBy, $vars, $lang->customer->nextDate);?></th>
+        <th class='w-100px'><?php commonModel::printOrderLink('createdDate',  $orderBy, $vars, $lang->customer->createdDate);?></th>
         <th class='w-100px'><?php echo $lang->actions;?></th>
       </tr>
     </thead>
@@ -37,13 +37,13 @@
       <tr class='text-center' data-url='<?php echo $this->createLink('customer', 'edit', "customerID=$customer->id"); ?>'>
         <td><?php echo $customer->id;?></td>
         <td class='text-left'><strong><?php echo $customer->name;?></strong></td>
-        <td><?php echo $customer->createdDate;?></td>
-        <td><?php echo $lang->customer->typeList[$customer->type];?></td>
+        <td><?php echo $customer->level;?></td>
         <td><?php echo $lang->customer->statusList[$customer->status];?></td>
         <td><?php echo $lang->customer->sizeList[$customer->size];?></td>
-        <td><?php echo $customer->level;?></td>
-        <td><?php echo $customer->contactedDate;?></td>
+        <td><?php echo $lang->customer->typeList[$customer->type];?></td>
+        <td><?php echo substr($customer->contactedDate, 0, 10);?></td>
         <td><?php echo $customer->nextDate;?></td>
+        <td><?php echo substr($customer->createdDate, 0, 10);?></td>
         <td class='actions'>
           <?php
           echo html::a(inlink('edit', "customerID=$customer->id"), $lang->edit);
