00501c1c38ee1a59554f00936b13d529a0cfb3e8
daitingting <daitingting@cnezsoft.com>
2014-04-25 04:57:54 +0000

    * rm unused code.
    git-svn-id: file:///tmp/ranzhi/trunk@928 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/customer/lang/en.php
+++ ranzhi/app/crm/customer/lang/en.php
@@ -42,7 +42,6 @@ $lang->customer->contract    = 'Contract List';
 $lang->customer->address     = 'Address List';
 $lang->customer->record      = 'Record';
 $lang->customer->linkContact = 'Create Contact';
-$lang->customer->basic       = 'Basic Information';
 
 $lang->customer->typeList['']           = '';
 $lang->customer->typeList['national']   = 'National';
