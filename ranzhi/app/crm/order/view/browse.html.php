d1146cec8e0c4c5a019e235f20ce6b2449ec02a4
chunsheng <chunsheng@cnezsoft.com>
2014-04-26 03:25:07 +0000

    * adjust the table layout of order list page.
    git-svn-id: file:///tmp/ranzhi/trunk@953 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/order/view/browse.html.php
+++ ranzhi/app/crm/order/view/browse.html.php
@@ -18,23 +18,23 @@
   </div>
   <table class='table table-hover table-striped tablesorter table-data'>
     <thead>
-      <tr>
+      <tr class='text-center'>
         <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
         <th class='w-60px text-center' ><?php commonModel::printOrderLink('id', $orderBy, $vars, $lang->order->id);?></th>
         <th class='w-40px'><?php echo $lang->customer->level;?></th>
         <th><?php commonModel::printOrderLink('customer', $orderBy, $vars, $lang->order->customer);?></th>
         <th><?php commonModel::printOrderLink('product', $orderBy, $vars, $lang->order->product);?></th>
         <th class='w-120px'><?php commonModel::printOrderLink('plan', $orderBy, $vars, $lang->order->plan);?>
-        <th class='w-120px'><?php commonModel::printOrderLink('assignedTo', $orderBy, $vars, $lang->order->assignedTo);?></th>
+        <th class='w-70px'><?php commonModel::printOrderLink('assignedTo', $orderBy, $vars, $lang->order->assignedTo);?></th>
         <th class='w-60px' ><?php commonModel::printOrderLink('status', $orderBy, $vars, $lang->order->status);?></th>
-        <th class='w-100px' ><?php commonModel::printOrderLink('contactedDate', $orderBy, $vars, $lang->order->contactedDate);?></th>
-        <th class='w-100px' ><?php commonModel::printOrderLink('nextDate', $orderBy, $vars, $lang->order->nextDate);?></th>
-        <th class='w-170px text-center'><?php echo $lang->actions;?></th>
+        <th class='w-80px' ><?php commonModel::printOrderLink('contactedDate', $orderBy, $vars, $lang->order->contactedDate);?></th>
+        <th class='w-80px' ><?php commonModel::printOrderLink('nextDate', $orderBy, $vars, $lang->order->nextDate);?></th>
+        <th class='w-180px text-center'><?php echo $lang->actions;?></th>
       </tr>
     </thead>
     <tbody>
       <?php foreach($orders as $order):?>
-      <tr data-url='<?php echo $this->createLink('order', 'view', "orderID=$order->id"); ?>'>
+      <tr class='text-center' data-url='<?php echo $this->createLink('order', 'view', "orderID=$order->id");?>'>
         <td class='text-center'><?php echo $order->id;?></td>
         <td class='text-center'><?php echo $lang->customer->levelList[$customers[$order->customer]->level];?></td>
         <td><?php echo $customers[$order->customer]->name;?></td>
