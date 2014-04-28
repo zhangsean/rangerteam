ecd9d5091d171b79d7e703e50abbee24b2cc7065
guanxiying <guanxiying@cnezsoft.com>
2014-04-28 07:45:22 +0000

    * disable assign button of closed order.
    git-svn-id: file:///tmp/ranzhi/trunk@982 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/order/view/view.html.php
+++ ranzhi/app/crm/order/view/view.html.php
@@ -19,7 +19,9 @@
     if($order->status == 'normal') echo html::a(helper::createLink('contract', 'create', "orderID=$order->id"), $this->lang->order->sign, "class='btn btn-default'");
     if($order->status != 'normal') echo html::a('###', $this->lang->order->sign, "class='btn' disabled='disabled' class='disabled'");
 
-    echo html::a(inlink('assign', "orderID=$order->id"), $this->lang->assign, "data-toggle='modal' class='btn btn-default'");
+    if($order->status != 'closed')  echo html::a(inlink('assign', "orderID=$order->id"), $this->lang->assign, "data-toggle='modal' class='btn btn-default'");
+    if($order->status == 'closed')  echo html::a('###', $this->lang->assign, "data-toggle='modal' class='btn btn-default disabled' disabled");
+
     echo html::a(inlink('edit',     "orderID=$order->id"), $this->lang->edit,   "class='btn btn-default'");
 
     if($order->status != 'closed') echo html::a(inlink('close', "orderID=$order->id"), $this->lang->close, "class='btn btn-default' data-toggle='modal'");
