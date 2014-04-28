ad85b97e2a8301e3403c367e1c188664e479ef5b
guanxiying <guanxiying@cnezsoft.com>
2014-04-28 09:22:20 +0000

    * rm view link.
    git-svn-id: file:///tmp/ranzhi/trunk@990 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/oa/announce/view/browse.html.php
+++ ranzhi/app/oa/announce/view/browse.html.php
@@ -16,7 +16,6 @@
     <div class='panel-heading'><h4><i class='icon-calendar'></i><?php echo $lang->announce->browse;?></h4></div>
     <section class='items items-hover'>
       <?php foreach($articles as $article):?>
-      <?php $url = inlink('view', "id=$article->id");?>
       <div class='item'>
         <div class='item-heading'>
           <div class="text-muted pull-right">
@@ -24,7 +23,7 @@
             <span title="<?php echo $lang->article->addedDate;?>"><i class='icon-time'></i> <?php echo substr($article->addedDate, 0, 10);?></span>&nbsp; 
             <span title="<?php echo $lang->article->views;?>"><i class='icon-eye-open'></i> <?php echo $article->views;?></span> &nbsp; 
           </div>
-          <h4><?php echo html::a($url, $article->title);?></h4>
+          <h4><?php echo $article->title;?></h4>
         </div>
         <div class='item-content'>
           <?php if(!empty($article->image)):?>
@@ -35,7 +34,7 @@
             ?>
           </div>
           <?php endif;?>
-          <div class='text'><?php echo helper::substr($article->content, 800);?></div>
+          <div class='text'><?php echo $article->content;?></div>
           <div class='text pull-right'>
             <?php echo html::a($this->createLink('article', 'edit', "articleID={$article->id}&type=announce"), $lang->edit);?>
             <?php echo html::a($this->createLink('article', 'delete', "articleID={$article->id}"), $lang->delete, "class='deleter'");?>
