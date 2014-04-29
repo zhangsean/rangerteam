6ab0e69ae035e47a462dafc76d09894af9cf643a
chunsheng <chunsheng@cnezsoft.com>
2014-04-29 08:43:22 +0000

    * remove the strong tag of contract list page.
    git-svn-id: file:///tmp/ranzhi/trunk@1027 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/contract/view/browse.html.php
+++ ranzhi/app/crm/contract/view/browse.html.php
@@ -35,7 +35,7 @@
       <?php foreach($contracts as $contract):?>
       <tr class='text-center' data-url='<?php echo inlink('view', "contractID=$contract->id"); ?>'>
         <td><?php echo $contract->id;?></td>
-        <td class='text-left'><strong><?php echo $contract->name;?></strong></td>
+        <td class='text-left'><?php echo $contract->name;?></td>
         <td><?php echo $customers[$contract->customer];?></td>
         <td><?php echo $contract->amount;?></td>
         <td><?php echo substr($contract->createdDate, 0, 10);?></td>
