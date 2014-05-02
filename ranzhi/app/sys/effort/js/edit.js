ad61d3d06815befa68df8bd3f80b54fb7fcaeae5
guanxiying <guanxiying@cnezsoft.com>
2014-05-02 02:20:00 +0000

    * remove it to ext.
    git-svn-id: file:///tmp/ranzhi/trunk@1128 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/sys/effort/js/edit.js
+++
@@ -1,8 +0,0 @@
-$(function()
-{
-    /* Set edit effort ajax form. */
-    $.setAjaxForm('#editEffortForm', function(data)
-    {
-        if(data.result == 'success') $.reloadAjaxModal(1500);
-    });
-})
