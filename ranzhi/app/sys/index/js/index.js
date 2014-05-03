1aea735e922db6fa5b4b9a8996d8a3a1a0992b64
daitingting <daitingting@cnezsoft.com>
2014-05-03 01:38:47 +0000

    * adjust code.
    git-svn-id: file:///tmp/ranzhi/trunk@1164 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/sys/index/js/index.js
+++ ranzhi/app/sys/index/js/index.js
@@ -2,7 +2,7 @@ $(function()
 {
     /* start ips */
     $.ipsStart(entries, $.extend({onBlocksOrdered: sortBlocks, onDeleteBlock: deleteBlock}, config, ipsLang));
-    $('.entries-count').text(entries.length - 2);
+    $('.entries-count').text(entries.length - 1);
 })
 
 /**
