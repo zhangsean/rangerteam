65e937a0bf05ad8713a27ca6fde62871df5f8c5c
daitingting <daitingting@cnezsoft.com>
2014-05-20 06:19:22 +0000

    * Add depositor, balance and trade tables.
    git-svn-id: file:///tmp/ranzhi/trunk@1171 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/db/upgrade1.0.sql
@@ -0,0 +1,48 @@
+CREATE TABLE `cash_depositor` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `abbr` char(60) NOT NULL,
+  `provider` char(100) NOT NULL,
+  `title` char(100) NOT NULL,
+  `account` char(90) NOT NULL,
+  `bankcode` varchar(30) NOT NULL,
+  `public` enum('0','1') NOT NULL,
+  `type` enum('cash','bank','online') NOT NULL,
+  `currency` char(30) NOT NULL,
+  `status` enum('normal','disable') NOT NULL DEFAULT 'normal',
+  `addedBy` char(30) NOT NULL DEFAULT '',
+  `addedDate` datetime NOT NULL,
+  `editedBy` char(30) NOT NULL DEFAULT '',
+  `editedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cash_balance` ( 
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `depositor` mediumint(8) NOT NULL,
+  `date` date NOT NULL,
+  `money` float(12,2) NOT NULL,
+  `currency` char(30) NOT NULL,
+  `addedBy` char(30) NOT NULL DEFAULT '',
+  `addedDate` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cash_trade` (
+  `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT, 
+  `depositor` mediumint(8) NOT NULL,
+  `parent` text NOT NULL,
+  `product` mediumint(8) NOT NULL,
+  `trader` char(100) NOT NULL,
+  `order` mediumint(8) NOT NULL,
+  `contract` mediumint(8) NOT NULL,
+  `dept` mediumint(8) unsigned NOT NULL,
+  `type` enum('in', 'out') NOT NULL,
+  `transfer` enum('0', '1') NOT NULL,
+  `money` float(12,2) NOT NULL,
+  `currency` varchar(30) NOT NULL,
+  `date` datetime NOT NULL,
+  `handler` char(30) NOT NULL,
+  `category` mediumint(8) NOT NULL,
+  `desc` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
