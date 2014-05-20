d7b557cb94cbc8663f0834a4b4e6ff065fdcdbb0
daitingting <daitingting@cnezsoft.com>
2014-05-20 07:53:33 +0000

    * Add en file.
    git-svn-id: file:///tmp/ranzhi/trunk@1179 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/cash/depositor/lang/en.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * The depositor module en file of RanZhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     depositor
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+$lang->depositor->id          = 'ID';
+$lang->depositor->abbr        = 'Abbreviation';
+$lang->depositor->provider    = 'Provider';
+$lang->depositor->title       = 'Title';
+$lang->depositor->account     = 'Account';
+$lang->depositor->bankcode    = 'Bank Code';
+$lang->depositor->public      = 'Public';
+$lang->depositor->type        = 'Type';
+$lang->depositor->currency    = 'Currency';
+$lang->depositor->status      = 'Status';
+$lang->depositor->createdBy   = 'Created By';
+$lang->depositor->createdDate = 'Created Date';
+$lang->depositor->editedBy    = 'Edited By';
+$lang->depositor->editedDate  = 'Edited Date';
+
+$lang->depositor->create  = 'Create';
+$lang->depositor->list    = 'List';
+$lang->depositor->edit    = 'Edit';
+$lang->depositor->view    = 'View';
+
+$lang->depositor->typeList['cash']   = 'Cash';
+$lang->depositor->typeList['bank']   = 'Bank';
+$lang->depositor->typeList['online'] = 'Online';
+
+$lang->depositor->publicList['1'] = 'Public';
+$lang->depositor->publicList['0'] = 'Personal';
