13ec65be2b6b5f02cdd620fd4f1e63a38b116820
daitingting <daitingting@cnezsoft.com>
2014-05-22 05:23:07 +0000

    * adjust browse function.
    git-svn-id: file:///tmp/ranzhi/trunk@1185 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/cash/depositor/control.php
+++ ranzhi/app/cash/depositor/control.php
@@ -32,13 +32,14 @@ class depositor extends control
      * @access public
      * @return void
      */
-    public function browse($orderBy = 'id_desc', $recTotal = 0, $recPerPage = 20, $pageID = 1)
+    public function browse($type = 'cash', $orderBy = 'id_desc', $recTotal = 0, $recPerPage = 20, $pageID = 1)
     {   
         $this->app->loadClass('pager', $static = true);
         $pager = new pager($recTotal, $recPerPage, $pageID);
 
         $this->view->title      = $this->lang->depositor->list;
-        $this->view->depositors = $this->depositor->getList($orderBy, $pager);
+        $this->view->depositors = $this->depositor->getList($type, $orderBy, $pager);
+        $this->view->type       = $type;
         $this->view->pager      = $pager;
         $this->view->orderBy    = $orderBy;
         $this->display();
--- ranzhi/app/cash/depositor/view/browse.html.php
+++ ranzhi/app/cash/depositor/view/browse.html.php
@@ -14,6 +14,12 @@
 <div class='panel'>
   <div class='panel-heading'>
     <strong><i class="icon-group"></i> <?php echo $lang->depositor->list;?></strong>
+    <?php
+    echo '&nbsp; &nbsp; &nbsp;';
+    echo html::a(inlink('browse', "type=cash"),   $lang->depositor->typeList['cash'],   $type == 'cash' ? "class='active'" : '');
+    echo html::a(inlink('browse', "type=bank"),   $lang->depositor->typeList['bank'],   $type == 'bank' ? "class='active'" : '');
+    echo html::a(inlink('browse', "type=online"), $lang->depositor->typeList['online'], $type == 'online' ? "class='active'" : '');
+    ?>
     <div class='panel-actions pull-right'>
       <?php echo html::a(inlink('create'), "<i class='icon-plus'>{$lang->depositor->create}</i>", "class='btn btn-primary'")?>
     </div>
@@ -21,16 +27,19 @@
   <table class='table table-hover table-striped tablesorter table-data' id='depositorList'>
     <thead>
       <tr class='text-center'>
-        <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
-        <th class='w-50px'> <?php commonModel::printOrderLink('id', $orderBy, $vars, $lang->depositor->id);?></th>
-        <th class='w-100px'><?php commonModel::printOrderLink('abbr', $orderBy, $vars, $lang->depositor->abbr);?></th>
+        <?php $vars = "type={$type}&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
+        <th class='w-50px'><?php commonModel::printOrderLink('id', $orderBy, $vars, $lang->depositor->id);?></th>
+        <th><?php commonModel::printOrderLink('abbr', $orderBy, $vars, $lang->depositor->abbr);?></th>
+        <?php if($type != 'cash'):?>
         <th class='w-100px'><?php commonModel::printOrderLink('provider', $orderBy, $vars, $lang->depositor->serviceProvider);?></th>
-        <th class='w-100px'> <?php commonModel::printOrderLink('title', $orderBy, $vars, $lang->depositor->title);?></th>
+        <th class='w-160px'> <?php commonModel::printOrderLink('title', $orderBy, $vars, $lang->depositor->title);?></th>
         <th class='w-100px'><?php commonModel::printOrderLink('account', $orderBy, $vars, $lang->depositor->account);?></th>
+        <?php if($type == 'bank'):?>
         <th class='w-100px'><?php commonModel::printOrderLink('bankcode', $orderBy, $vars, $lang->depositor->bankcode);?></th>
-        <th class='w-60px'><?php commonModel::printOrderLink('public', $orderBy, $vars, $lang->depositor->public);?></th>
-        <th class='w-60px'><?php commonModel::printOrderLink('type', $orderBy, $vars, $lang->depositor->type);?></th>
-        <th class='w-60px'><?php commonModel::printOrderLink('currency', $orderBy, $vars, $lang->depositor->currency);?></th>
+        <?php endif;?>
+        <th class='w-80px'><?php commonModel::printOrderLink('public', $orderBy, $vars, $lang->depositor->public);?></th>
+        <?php endif;?>
+        <th class='w-80px'><?php commonModel::printOrderLink('currency', $orderBy, $vars, $lang->depositor->currency);?></th>
         <th class='w-60px'><?php commonModel::printOrderLink('status', $orderBy, $vars, $lang->depositor->status);?></th>
         <th class='w-80px'><?php echo $lang->actions;?></th>
       </tr>
@@ -40,12 +49,18 @@
     <tr class='text-center'>
       <td><?php echo $depositor->id;?></td>
       <td><?php echo $depositor->abbr;?></td>
-      <td><?php echo $depositor->provider;?></td>
+      <?php if($type != 'cash'):?>
+      <td>
+        <?php if($depositor->type == 'online') echo $lang->depositor->providerList[$depositor->provider];?>
+        <?php if($depositor->type == 'bank')   echo $depositor->provider;?>
+      </td>
       <td><?php echo $depositor->title;?></td>
       <td><?php echo $depositor->account;?></td>
+      <?php if($type == 'bank'):?>
       <td><?php echo $depositor->bankcode;?></td>
+      <?php endif;?>
       <td><?php echo $lang->depositor->publicList[$depositor->public];?></td>
-      <td><?php echo $lang->depositor->typeList[$depositor->type];?></td>
+      <?php endif;?>
       <td><?php echo $depositor->currency;?></td>
       <td><?php echo $lang->depositor->statusList[$depositor->status];?></td>
       <td class='operate'>
