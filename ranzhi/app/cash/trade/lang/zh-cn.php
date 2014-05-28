82d550bb0720e7f8493c093a69d9e81e4b8ed1f8
daitingting <daitingting@cnezsoft.com>
2014-05-28 09:05:40 +0000

    * adjust transfer function.
    git-svn-id: file:///tmp/ranzhi/trunk@1209 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/cash/trade/lang/zh-cn.php
+++ ranzhi/app/cash/trade/lang/zh-cn.php
@@ -9,24 +9,26 @@
  * @version     $Id$
  * @link        http://www.ranzhi.org
  */
-$lang->trade->id        = '编号';
-$lang->trade->depositor = '账号';
-$lang->trade->type      = '交易类型';
-$lang->trade->currency  = '货币类型';
-$lang->trade->trader    = '商户';
-$lang->trade->customer  = '客户';
-$lang->trade->money     = '金额';
-$lang->trade->desc      = '说明';
-$lang->trade->product   = '产品';
-$lang->trade->order     = '订单';
-$lang->trade->contract  = '合同';
-$lang->trade->category  = '科目';
-$lang->trade->date      = '时间';
-$lang->trade->handler   = '经办人';
-$lang->trade->dept      = '部门';
-$lang->trade->receipt   = '收款账户';
-$lang->trade->payment   = '付款账户';
-$lang->trade->fee       = '手续费';
+$lang->trade->id          = '编号';
+$lang->trade->depositor   = '账号';
+$lang->trade->type        = '交易类型';
+$lang->trade->currency    = '货币类型';
+$lang->trade->trader      = '商户';
+$lang->trade->customer    = '客户';
+$lang->trade->money       = '金额';
+$lang->trade->desc        = '说明';
+$lang->trade->product     = '产品';
+$lang->trade->order       = '订单';
+$lang->trade->contract    = '合同';
+$lang->trade->category    = '科目';
+$lang->trade->date        = '时间';
+$lang->trade->handler     = '经办人';
+$lang->trade->dept        = '部门';
+$lang->trade->receipt     = '收款账户';
+$lang->trade->payment     = '付款账户';
+$lang->trade->fee         = '手续费';
+$lang->trade->transferIn  = '转入金额';
+$lang->trade->transferOut = '转出金额';
 
 $lang->trade->in       = '收入';
 $lang->trade->out      = '支出';
@@ -38,3 +40,4 @@ $lang->trade->typeList['in']  = '收入';
 $lang->trade->typeList['out'] = '支出';
 
 $lang->trade->notEqual = '付款账号不能与收款账号相同。';
+$lang->trade->feeDesc  = '%s从%s转出%s%s元，转入%s%s%s元';
