1692c170ccffda6568bab85dede4343a1bc7f7ef
daitingting <daitingting@cnezsoft.com>
2014-06-03 07:26:20 +0000

    * adjust the style of browse page.
    git-svn-id: file:///tmp/ranzhi/trunk@1249 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/cash/depositor/view/browse.html.php
+++ ranzhi/app/cash/depositor/view/browse.html.php
@@ -21,34 +21,30 @@
   <div class='panel-body'>
     <?php foreach($depositors as $depositor):?>
     <div class='col-md-4'>
-    <div class='panel'>
-      <table class='table'>
-        <tr>
-          <td>
-            <div class='lead'><?php echo $depositor->abbr;?></div>
-            <div class='info'>
-            <?php echo "<div><strong>{$lang->depositor->type} {$lang->colon} </strong>{$lang->depositor->typeList[$depositor->type]}</div>";?>
-            <?php if($depositor->type != 'cash'):?>
-            <?php echo "<div><strong>{$lang->depositor->title} {$lang->colon} </strong>$depositor->title</div>";?>
-            <?php if($depositor->type == 'bank') echo "<div><strong>{$lang->depositor->bankProvider} {$lang->colon} </strong>$depositor->provider </div>";?>
-            <?php if($depositor->type == 'online') echo "<div><strong>{$lang->depositor->serviceProvider} {$lang->colon} </strong>{$lang->depositor->providerList[$depositor->provider]} </div>";?>
-            <?php echo "<div><strong>{$lang->depositor->account} {$lang->colon} </strong>$depositor->account</div>";?>
-            <?php if($depositor->type == 'bank') echo "<div><strong>{$lang->depositor->bankcode} {$lang->colon} </strong>$depositor->bankcode</div>";?>
-            <?php if($depositor->type != 'cash') echo "<div><strong>{$lang->depositor->public} {$lang->colon} </strong>{$lang->depositor->publicList[$depositor->public]}</div>";?>
-            <?php endif;?>
-            <?php echo "<div><strong>{$lang->depositor->currency} {$lang->colon} </strong>{$lang->depositor->currencyList[$depositor->currency]}</div>";?>
-            <?php echo "<div><strong>{$lang->depositor->status} {$lang->colon} </strong>{$lang->depositor->statusList[$depositor->status]}</div>";?>
-            </div>
-            <div class='pull-right'>
-              <?php echo html::a(inlink('edit', "depositorID=$depositor->id"), $lang->edit, "data-toggle='modal'");?>
-              <?php echo html::a(inlink('check', "depositorID=$depositor->id"), $lang->depositor->check);?>
-              <?php if($depositor->status == 'normal') echo html::a(inlink('forbid', "depositorID=$depositor->id"), $lang->depositor->forbid, "data-toggle=modal");?>
-              <?php if($depositor->status == 'disable') echo html::a(inlink('activate', "depositorID=$depositor->id"), $lang->depositor->activate, "data-toggle=modal");?>
-            </div>
-          </td>
-        </tr>
-      </table>
-    </div>
+      <div class='panel'>
+        <div class='panel-body'>
+          <div class='lead'><?php echo $depositor->abbr;?></div>
+          <div class='info'>
+          <?php echo "<dl class='dl-horizontal'><dt>{$lang->depositor->type} {$lang->colon} </dt><dd>{$lang->depositor->typeList[$depositor->type]}</dd></dl>";?>
+          <?php if($depositor->type != 'cash'):?>
+          <?php echo "<dl class='dl-horizontal'><dt>{$lang->depositor->title} {$lang->colon} </dt><dd>$depositor->title</dd></dl>";?>
+          <?php if($depositor->type == 'bank') echo "<dl class='dl-horizontal'><dt>{$lang->depositor->bankProvider} {$lang->colon} </dt><dd>$depositor->provider </dd></dl>";?>
+          <?php if($depositor->type == 'online') echo "<dl class='dl-horizontal'><dt>{$lang->depositor->serviceProvider} {$lang->colon} </dt><dd>{$lang->depositor->providerList[$depositor->provider]} </dd></dl>";?>
+          <?php echo "<dl class='dl-horizontal'><dt>{$lang->depositor->account} {$lang->colon} </dt><dd>$depositor->account</dd></dl>";?>
+          <?php if($depositor->type == 'bank') echo "<dl class='dl-horizontal'><dt>{$lang->depositor->bankcode} {$lang->colon} </dt><dd>$depositor->bankcode</dd></dl>";?>
+          <?php if($depositor->type != 'cash') echo "<dl class='dl-horizontal'><dt>{$lang->depositor->public} {$lang->colon} </dt><dd>{$lang->depositor->publicList[$depositor->public]}</dd></dl>";?>
+          <?php endif;?>
+          <?php echo "<dl class='dl-horizontal'><dt>{$lang->depositor->currency} {$lang->colon} </dt><dd>{$lang->depositor->currencyList[$depositor->currency]}</dd></dl>";?>
+          <?php echo "<dl class='dl-horizontal'><dt>{$lang->depositor->status} {$lang->colon} </dt><dd>{$lang->depositor->statusList[$depositor->status]}</dd></dl>";?>
+          </div>
+          <div class='pull-right'>
+            <?php echo html::a(inlink('edit', "depositorID=$depositor->id"), $lang->edit, "data-toggle='modal'");?>
+            <?php echo html::a(inlink('check', "depositorID=$depositor->id"), $lang->depositor->check);?>
+            <?php if($depositor->status == 'normal') echo html::a(inlink('forbid', "depositorID=$depositor->id"), $lang->depositor->forbid, "data-toggle=modal");?>
+            <?php if($depositor->status == 'disable') echo html::a(inlink('activate', "depositorID=$depositor->id"), $lang->depositor->activate, "data-toggle=modal");?>
+          </div>
+        </div>
+      </div>
     </div>
     <?php endforeach;?>
   </div>
