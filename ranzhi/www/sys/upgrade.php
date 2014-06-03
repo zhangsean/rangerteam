8590790fe2bdc857bb716fe4d6c430b92c461ee4
guanxiying <guanxiying@cnezsoft.com>
2014-06-03 07:19:40 +0000

    * fix upgrade bug.
    git-svn-id: file:///tmp/ranzhi/trunk@1246 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/www/sys/upgrade.php
+++ ranzhi/www/sys/upgrade.php
@@ -11,11 +11,11 @@
  */
 /* Judge my.php exists or not. */
 define('RUN_MODE', 'upgrade');
-$myConfig = dirname(dirname(__FILE__)) . '/config/my.php';
+$myConfig = dirname(dirname(dirname(__FILE__))) . '/config/my.php';
 if(!file_exists($myConfig))
 {
-    echo "文件" . $myConfig . "不存在！ 提示：不要重命名原来的禅道安装目录，下载最新的源码包，覆盖即可。" . "<br />";
-    echo $myConfig . " doesn't exists! Please don't rename zentao before overriding the source code!";
+    echo "文件" . $myConfig . "不存在！ 提示：不要重命名原来的然之安装目录，下载最新的源码包，覆盖即可。" . "<br />";
+    echo $myConfig . " doesn't exists! Please don't rename ranzhi before overriding the source code!";
     exit;
 }
 
@@ -38,9 +38,8 @@ $app->setDebug();
 
 /* Check the installed version is the latest or not. */
 $config->installedVersion = $common->loadModel('setting')->getVersion();
-if(!$_SERVER['HTTP_X_REQUESTED_WITH'] and version_compare($config->version, $config->installedVersion) <= 0) die(header('location: ../index.php'));
+if(version_compare($config->version, $config->installedVersion) <= 0) die(header('location: ../index.php'));
 
 /* Run it. */
 $app->parseRequest();
-$common->checkUpgradeStatus();
 $app->loadModule();
