2883e3bcdd7d2dbb5311c6e6194c5d39713ee40b
guanxiying <guanxiying@cnezsoft.com>
2014-06-03 10:54:26 +0000

    * use in and out.
    git-svn-id: file:///tmp/ranzhi/trunk@1261 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/cash/common/lang/zh-cn.php
+++ ranzhi/app/cash/common/lang/zh-cn.php
@@ -33,6 +33,6 @@ $lang->trade->menu->browse   = array('link' => '<i class="icon-th-list"></i> 列
 /* Menu of setting module. */
 $lang->setting = new stdclass();
 $lang->setting->menu = new stdclass();
-$lang->setting->menu->income   = '收入科目|tree|browse|type=income|';
-$lang->setting->menu->expense  = '支出科目|tree|browse|type=expense|';
+$lang->setting->menu->income   = '收入科目|tree|browse|type=in|';
+$lang->setting->menu->expense  = '支出科目|tree|browse|type=out|';
 $lang->setting->menu->currency = '货币类型|setting|lang|module=depositor&field=currencyList';
