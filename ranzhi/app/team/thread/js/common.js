0354fc5e8d1d533e46bb7d5753b8375abff5548f
daitingting <daitingting@cnezsoft.com>
2014-06-06 03:20:23 +0000

    * adjust highlight.
    git-svn-id: file:///tmp/ranzhi/trunk@1320 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/team/thread/js/common.js
+++ ranzhi/app/team/thread/js/common.js
@@ -3,5 +3,6 @@ $(document).ready(function()
     $('.file-form .form-group div[class*=col-sm]').removeAttr('style');
     $("a[href*='forum']").parent().addClass('active');
     $("a[href$='forum']").parent().removeClass('active');
-    $("a[id!='board" + v.boardID + "']").parent().removeClass('active');
+    $("a[id*='board']").parent().removeClass('active');
+    $("a[id='board" + v.boardID + "']").parent().addClass('active');
 });
