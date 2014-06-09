910ae514c25b7a6f025d4d344ef72da67cbcc423
daitingting <daitingting@cnezsoft.com>
2014-06-09 07:07:03 +0000

    * format code.
    git-svn-id: file:///tmp/ranzhi/trunk@1331 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/team/blog/control.php
+++ ranzhi/app/team/blog/control.php
@@ -18,14 +18,12 @@ class blog extends control
 
         $this->app->loadClass('pager', $static = true);
         $pager = new pager(0, 8, 1);
+        
         $this->view->latestArticles = $this->loadModel('article', 'sys')->getList('blog', 0, null, null, 'id_desc');
-
-        $this->view->authors = $this->loadModel('article', 'sys')->getAuthorList('blog');
-        $this->view->months  = $this->loadModel('article', 'sys')->getMonthList('blog');
-        $this->view->tags    = $this->loadModel('article', 'sys')->getTagList('blog');
-
+        $this->view->authors        = $this->loadModel('article', 'sys')->getAuthorList('blog');
+        $this->view->months         = $this->loadModel('article', 'sys')->getMonthList('blog');
+        $this->view->tags           = $this->loadModel('article', 'sys')->getTagList('blog');
         $this->view->latestComments = $this->loadModel('message', 'sys')->getList('comment', 'blog', '');
-
     }
 
     /** 
