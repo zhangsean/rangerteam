32259811f0a25dbfa2b2d7e7fc2cafff9ab64597
daitingting <daitingting@cnezsoft.com>
2014-06-13 03:27:15 +0000

    * add file and action module rights.
    git-svn-id: file:///tmp/ranzhi/trunk@1378 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/config/rights.php
+++ ranzhi/config/rights.php
@@ -36,13 +36,34 @@ $config->rights->member['rss']['index']              = 'index';
 $config->rights->member['sitemap']['index']          = 'index';
 $config->rights->member['links']['index']            = 'index';
 $config->rights->member['error']['index']            = 'index';
+
 $config->rights->member['misc']['qrcode']            = 'qrcode';
 $config->rights->member['user']['setreferer']        = 'setreferer';
 $config->rights->member['contract']['getorder']      = 'getorder';
 $config->rights->member['customer']['getoptionmenu'] = 'getoptionmenu';
 $config->rights->member['contact']['getoptionmenu']  = 'getoptionmenu';
 $config->rights->member['thread']['locate']          = 'locate';
+
 $config->rights->member['reply']['post']             = 'post';
 $config->rights->member['reply']['edit']             = 'edit';
 $config->rights->member['reply']['delete']           = 'delete';
 $config->rights->member['reply']['deletefile']       = 'deletefile';
+
+$config->rights->member['action']['createrecord']    = 'createrecord';
+$config->rights->member['action']['editrecord']      = 'editrecord';
+$config->rights->member['action']['history']         = 'history';
+$config->rights->member['action']['editcomment']     = 'editcomment';
+
+$config->rights->member['file']['buildform']         = 'buildform';
+$config->rights->member['file']['buildlist']         = 'buildlist';
+$config->rights->member['file']['printfiles']        = 'printfiles';
+$config->rights->member['file']['ajaxupload']        = 'ajaxupload';
+$config->rights->member['file']['browse']            = 'browse';
+$config->rights->member['file']['edit']              = 'edit';
+$config->rights->member['file']['upload']            = 'upload';
+$config->rights->member['file']['download']          = 'download';
+$config->rights->member['file']['senddownheader']    = 'senddownheader';
+$config->rights->member['file']['delete']            = 'delete';
+$config->rights->member['file']['ajaxpasteimage']    = 'ajaxpasteimage';
+$config->rights->member['file']['filemanager']       = 'filemanager';
+$config->rights->member['file']['sort']              = 'sort';
