15c70f0957b5f5fcf79bfd55f7e712d5c74adba9
sunhao <sunhao@cnezsoft.com>
2014-06-13 13:23:15 +0000

    * ajusted views of kanban.
    git-svn-id: file:///tmp/ranzhi/trunk@1398 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/sys/task/view/kanban.html.php
+++ ranzhi/app/sys/task/view/kanban.html.php
@@ -11,39 +11,48 @@
  */
 ?>
 <?php include $app->getModuleRoot() . 'common/view/header.html.php';?>
-<div class="boards task-boards">
-  <?php foreach ($lang->task->statusList as $key => $value):?>
-  <?php if(empty($key)) continue; ?>
-  <div class="board panel task-board">
-    <div class="panel-heading">
-      <strong><?php echo $value?></strong>
-      <div class="panel-actions pull-right"><button class="btn"><i class="icon-plus"></i></button></div>
-    </div>
-    <div class="panel-body">
-      <div class="board-list">
-        <?php foreach($tasks as $task):?>
-        <?php if($task->status != $key) continue;?>
-        <div class="board-item task task-pri-<?php echo $task->pri; ?>">
-          <div class="task-heading">
-            <?php if(!empty($task->type)):?>
-            <div class="pull-right text-muted task-type"><?php echo $lang->task->typeList[$task->type];?></div>
-            <?php endif;?>
-            <strong class='task-name'><?php echo $task->name;?></strong>
-          </div>
-          <div class="task-info clearfix">
-          <?php if(!empty($task->deadline) and $task->deadline != '0000-00-00'):?>
-            <div class="task-deadline text-warning pull-left"><i class="icon-time"></i> <small><?php echo $task->deadline;?></small></div>
-            <div class="task-assignedTo text-muted pull-right"><i class="icon-user"></i> <small><?php echo $task->assignedTo;?></small></div>
-          <?php endif;?>
-          </div>
-          <div class="task-actions">
-            <?php echo $this->task->buildOperateMenu($task);?>
+<div class='panel'>
+  <div class='panel-heading'><i class='icon-list-alt'></i> <strong><?php echo $lang->task->kanban;?></strong></div>
+  <div class='panel-body boards-container'>
+    <div class='boards task-boards'>
+    <?php $colsWidth = 100/(count($lang->task->statusList) - 1); ?>
+    <?php foreach ($lang->task->statusList as $key => $value):?>
+    <?php if(empty($key)) continue; ?>
+      <div class='board panel task-board' style="width: <?php echo $colsWidth;?>%">
+        <div class='panel-heading'>
+          <strong><?php echo $value?></strong>
+          <div class='panel-actions pull-right'><button class='btn'><i class='icon-plus'></i></button></div>
+        </div>
+        <div class='panel-body'>
+          <div class='board-list'>
+            <?php foreach($tasks as $task):?>
+            <?php if($task->status != $key) continue;?>
+            <div class='board-item task task-pri-<?php echo $task->pri; ?>'>
+              <div class='task-heading'>
+                <?php if(!empty($task->type)):?>
+                <div class='pull-right text-muted task-type'><?php echo $lang->task->typeList[$task->type];?></div>
+                <?php endif;?>
+                <strong class='task-name'><?php echo $task->name;?></strong>
+              </div>
+              <div class='task-info clearfix'>
+              <?php if(!empty($task->assignedTo)):?>
+                <div class='task-assignedTo text-muted pull-left'><i class='icon-user'></i> <small><?php echo $task->assignedTo;?></small></div>
+              <?php endif;?>
+              <?php if(!empty($task->deadline) and $task->deadline != '0000-00-00'):?>
+                <div class='task-deadline text-warning pull-right'><i class='icon-time'></i> <small><?php echo $task->deadline;?></small></div>
+              <?php endif;?>
+              </div>
+              <div class='task-actions'>
+                <?php echo $this->task->buildOperateMenu($task);?>
+              </div>
+            </div>
+            <?php endforeach;?>
           </div>
         </div>
-        <?php endforeach;?>
       </div>
+    <?php endforeach;?>
+      <!-- <div class='board panel board-empty'></div> -->
     </div>
   </div>
-  <?php endforeach;?>
 </div>
 <?php include $app->getModuleRoot() . 'common/view/footer.html.php';?>
