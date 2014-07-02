cdfddebca4c41e413c110c1ae5db31050ce979f3
daitingting <daitingting@cnezsoft.com>
2014-07-02 01:18:40 +0000

    * Adjust the highlight.
    git-svn-id: file:///tmp/ranzhi/trunk@1418 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/team/reply/js/edit.js
+++ ranzhi/app/team/reply/js/edit.js
@@ -2,7 +2,7 @@ $(document).ready(function()
 {
     $('.file-form .form-group div[class*=col-sm]').removeAttr('style');
     $("a[href*='forum']").parent().addClass('active');
-    $("a[href$='forum']").parent().removeClass('active');
+    $("a[href*='forum'][href*='tree']").parent().removeClass('active');
     $("a[id*='board']").parent().removeClass('active');
     $("a[id='board" + v.boardID + "']").parent().addClass('active');
 });
