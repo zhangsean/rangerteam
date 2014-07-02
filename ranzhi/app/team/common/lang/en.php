380a4b5818fe002696303b4435db032494ea63cb
daitingting <daitingting@cnezsoft.com>
2014-07-02 02:26:06 +0000

    * Move the forum before blog.
    git-svn-id: file:///tmp/ranzhi/trunk@1422 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/team/common/lang/en.php
+++ ranzhi/app/team/common/lang/en.php
@@ -14,8 +14,8 @@ $lang->app->name = 'TEAM';
 
 $lang->menu->team = new stdclass();
 $lang->menu->team->dashboard = 'Dashboard|dashboard|index|';
-$lang->menu->team->blog      = 'Blog|blog|index|';
 $lang->menu->team->forum     = 'Forum|forum|index|';
+$lang->menu->team->blog      = 'Blog|blog|index|';
 $lang->menu->team->user      = 'Colleague|user|colleague|';
 $lang->menu->team->setting   = 'Setting|tree|browse|type=forum|';
 
