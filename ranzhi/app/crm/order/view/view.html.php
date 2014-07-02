841cb5a054b62415a0b9e91bcf0328c9f20117c0
daitingting <daitingting@cnezsoft.com>
2014-07-02 09:24:17 +0000

    * Add related link.
    git-svn-id: file:///tmp/ranzhi/trunk@1441 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/order/view/view.html.php
+++ ranzhi/app/crm/order/view/view.html.php
@@ -46,7 +46,7 @@
       <table class='table table-info'>
         <tr>
           <th class='w-80px'><?php echo $lang->order->customer;?></th>
-          <td><?php echo $customer->name . $lang->customer->levelList[$customer->level];?></td>
+          <td><?php echo html::a($this->createLink('customer', 'view', "customerID={$customer->id}"), $customer->name) . $lang->customer->levelList[$customer->level];?></td>
         </tr>
         <tr>
           <th><?php echo $lang->order->product;?></th>
