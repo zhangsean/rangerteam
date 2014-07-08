11c3253611e78003f1213759ecf8e250aa00d181
sunhao <sunhao@cnezsoft.com>
2014-07-08 05:47:58 +0000

    * ajusted style of contact/browse.  * fixed loading icon of iframe modal.
    git-svn-id: file:///tmp/ranzhi/trunk@1516 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/contact/view/browse.html.php
+++ ranzhi/app/crm/contact/view/browse.html.php
@@ -50,7 +50,7 @@
         <?php echo html::a($this->createLink('address', 'browse', "objectType=contact&objectID=$contact->id"), $lang->contact->address, "data-toggle='modal'");?>
         <?php echo html::a($this->createLink('resume', 'browse', "contactID=$contact->id"), $lang->contact->resume, "data-toggle='modal'");?>
         <?php echo html::a($this->createLink('contact', 'delete', "contactID=$contact->id"), $lang->delete, "class='reloadDeleter'");?>
-        <?php echo html::a($this->createLink('contact', 'vcard', "contactID=$contact->id"), $lang->contact->qrcode, "class='iframe' data-width='390'");?>
+        <?php echo html::a($this->createLink('contact', 'vcard', "contactID=$contact->id"), $lang->contact->qrcode, "class='iframe' data-width='390' data-icon='qrcode'");?>
       </td>
     </tr>
     <?php endforeach;?>
