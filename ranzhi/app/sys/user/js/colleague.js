cd97acf66fc017d64d9b1fbc08351b43537787dd
guanxiying <guanxiying@cnezsoft.com>
2014-07-08 00:47:43 +0000

    * use hover insteadof click.
    git-svn-id: file:///tmp/ranzhi/trunk@1501 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/sys/user/js/colleague.js
+++ ranzhi/app/sys/user/js/colleague.js
@@ -1,11 +1,18 @@
 $(document).ready(function()
 {
-    $('.btn-vcard').click(function()
+    $('.btn-vcard').hover(function()
     {
         $(this).parents('td').find('.contact-info, p.vcard').toggle();
         $(this).toggleClass('icon-qrcode');
         $(this).toggleClass('icon-list');
     });
+    $('.btn-vcard').blur(function()
+    {
+        $(this).parents('td').find('.contact-info, p.vcard').toggle();
+        $(this).toggleClass('icon-qrcode');
+        $(this).toggleClass('icon-list');
+    });
+
     $('p.vcard').hide();
     return false;
 });
