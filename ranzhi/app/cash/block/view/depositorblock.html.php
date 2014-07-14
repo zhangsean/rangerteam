146c7a26d8e2b3340254905794300aad3ae6b4e3
daitingting <daitingting@cnezsoft.com>
2014-07-14 09:35:31 +0000

    * Adjust style of block.
    git-svn-id: file:///tmp/ranzhi/trunk@1601 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/cash/block/view/depositorblock.html.php
+++ ranzhi/app/cash/block/view/depositorblock.html.php
@@ -10,22 +10,16 @@
  * @link        http://www.zentao.net
  */
 ?>
-<table class='table table-data table-hover block-depositor table-fixed'>
-  <tr>
-    <th class='w-160px'><?php echo $lang->depositor->title;?></th>
-    <th><?php echo $lang->depositor->serviceProvider . '/' . $lang->depositor->bankProvider;?></th>
-    <th class='w-130px'><?php echo $lang->depositor->account;?></th>
-  </tr>
-  <?php foreach($depositors as $id => $depositor):?>
-  <tr>
-    <td><?php echo $depositor->title;?></td>
-    <?php if($depositor->type == 'bank'):?>
-    <td><?php echo $depositor->provider;?></td>
-    <?php else:?>
-    <td><?php echo $lang->depositor->providerList[$depositor->provider];?></td>
-    <?php endif;?>
-    <td><?php echo $depositor->account;?></td>
-  </tr>
-  <?php endforeach;?>
-</table>
-<script>$('.block-depositor').dataTable();</script>
+<div class='article-content'>
+    <?php foreach($depositors as $id => $depositor):?>
+    <dl>
+      <?php if($depositor->type == 'bank'):?>
+      <dt><?php echo $depositor->title;?></dt>
+      <dd><?php echo $depositor->provider . ' ' . $depositor->account;?></dd>
+      <?php else:?>
+      <dt><?php echo $depositor->title;?></dt>
+      <dd><?php echo $lang->depositor->providerList[$depositor->provider] . ' ' . $depositor->account;?></dd>
+      <?php endif;?>
+    </dl>
+    <?php endforeach;?>
+</div>
