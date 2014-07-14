eeb48539872474c77c42cf3d61fa24b33c62fd1b
wangyidong <wangyidong@cnezsoft.com>
2014-07-14 02:53:30 +0000

    * add for task #2319.
    git-svn-id: file:///tmp/ranzhi/trunk@1591 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/db/upgrade1.2.beta.sql
+++ ranzhi/db/upgrade1.2.beta.sql
@@ -79,3 +79,7 @@ CREATE TABLE `sys_schema` (
 ALTER TABLE `sys_block` ADD `hidden` tinyint(1) unsigned NOT NULL DEFAULT '0';
 
 ALTER TABLE sys_user CHANGE avatar avatar varchar(255) NOT NULL;
+
+INSERT INTO `sys_schema` (`name`, `category`, `customer`, `type`, `money`, `desc`, `date`, `fee`) VALUES
+('支付宝',       '', 'H', 'K', 'J',   'I,O', 'D', 'M'),
+('中信银行简版', '', 'C', '',  'E,D', 'G,H', 'A', '');
