6e0cee3957542b997aadf99b37e125247f1b460e
guanxiying <guanxiying@cnezsoft.com>
2014-07-15 09:32:59 +0000

    * add batch edit view.
    git-svn-id: file:///tmp/ranzhi/trunk@1630 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ ranzhi/app/cash/trade/view/batchedit.html.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The batch edit trade view of trade module of RanZhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Xiying Guan <guanxiying@xirangit.com>
+ * @package     trade
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+?>
+<?php include $app->getModuleRoot() . 'common/view/header.html.php';?>
+<?php include '../../../sys/common/view/datepicker.html.php';?>
+<?php include '../../../sys/common/view/chosen.html.php';?>
+<form id='ajaxForm' method='post' action="<?php echo inlink('batchedit', 'step=save')?>">
+  <div class='panel'>
+    <div class='panel-heading'><strong><?php echo $lang->trade->showImport;?></strong></div>
+    <table class='table table-hover'>
+      <thead>
+        <tr class='text-center'>
+          <th class='w-100px'><?php echo $lang->trade->depositor;?></th>
+          <th class='w-100px'><?php echo $lang->trade->type;?></th> 
+          <th class='w-120px'><?php echo $lang->trade->category;?></th> 
+          <th class='w-180px'><?php echo $lang->trade->trader;?></th> 
+          <th class='w-120px'><?php echo $lang->trade->money;?></th>
+          <th class='w-80px'><?php echo $lang->trade->dept;?></th>
+          <th class='w-200px'><?php echo $lang->trade->handlers;?></th>
+          <th class='w-120px'><?php echo $lang->trade->date;?></th>
+          <th><?php echo $lang->trade->desc;?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($trades as $id => $trade):?>
+        <tr>
+          <td class='text-middle'><?php echo html::select("depositor[{$id}]", $depositors, $trade->depositor, "class='form-control' id='depositor{$id}'");?></td>
+          <td><?php echo html::select("type[{$id}]", $lang->trade->typeList, $trade->type, "class='form-control' readonly");?></td>
+          <td>
+            <?php if($trade->type == 'in') echo html::select("category[$id]", $incomeTypes, $trade->category, "class='form-control in' id='category{$id}'");?>
+            <?php if($trade->type == 'out') echo html::select("category[$id]", $expenseTypes, $trade->category, "class='form-control in' id='category{$id}'");?>
+          </td>
+          <td>
+            <?php if($trade->type == 'in') echo html::select("trader[{$id}]", $customerList, $trade->trader, "class='form-control'");?>
+            <?php if($trade->type == 'out') echo html::select("trader[{$id}]", $traderList, $trade->trader, "class='form-control'");?>
+          </td>
+          <td><?php echo html::input("money[$id]", $trade->money, "class='form-control' id='money{$id}'");?></td>
+          <td><?php echo html::select("dept[$id]", $deptList, '', "class='form-control chosen' id='dept{$id}'");?></td>
+          <td><?php echo html::select("handlers[$id][]", $users, $this->app->user->account, "class='form-control chosen' id='handlers{$id}' multiple");?></td>
+          <td><?php echo html::input("date[$id]", $trade->date, "class='form-control form-date' id='date{$id}'");?></td>
+          <td><?php echo html::textarea("desc[$id]", $trade->desc, "rows='1' class='form-control'");?></td>
+        </tr>
+        <?php endforeach;?>
+      </tbody>
+      <tfoot><tr><td colspan='9' class='text-center'><?php echo html::submitButton() . html::backButton();?></td></tr></tfoot>
+    </table>
+  </div>
+</form>
+<?php include $app->getModuleRoot() . 'common/view/footer.html.php';?>
