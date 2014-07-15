a54a46eaa6cbaad1089ea41d0755e1177257f469
wangyidong <wangyidong@cnezsoft.com>
2014-07-15 07:14:34 +0000

    * code for high light.
    git-svn-id: file:///tmp/ranzhi/trunk@1620 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- ranzhi/app/crm/order/js/common.js
+++ ranzhi/app/crm/order/js/common.js
@@ -1,6 +1,5 @@
 $(function()
 {
-    if(typeof(v.mode) == 'undefined') v.mode = 'all';
     $('.leftmenu li').removeClass('active').find('[href*=' + v.mode + ']').parent().addClass('active');
     $('.leftmenu li').find('[href*=' + v.mode + ']').parent().addClass('active');
 });
