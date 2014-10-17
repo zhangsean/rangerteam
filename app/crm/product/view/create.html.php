3412461b8938c061c238e91f3b69abdfc08ba21f
daitingting <daitingting@cnezsoft.com>
2014-10-17 06:37:15 +0000

    * rm extra code.
    git-svn-id: file:///tmp/ranzhi/trunk@1881 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/product/view/create.html.php
+++ app/crm/product/view/create.html.php
@@ -11,32 +11,30 @@
  */
 ?>
 <?php include '../../../sys/common/view/header.modal.html.php';?>
-<div class='modal-body'>
-  <form method='post' action='<?php echo inlink('create');?>' id='ajaxForm'>
-    <table class='table table-form'>
-      <tr>
-        <th class='w-50px'><?php echo $lang->product->name;?></th>
-        <td><?php echo html::input('name', '', "class='form-control'");?></td><td></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->product->type;?></th>
-        <td><?php echo html::select("type", $lang->product->typeList, '', "class='form-control'");?></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->product->status;?></th>
-        <td><?php echo html::select("status", $lang->product->statusList, '', "class='form-control'");?></td>
-      </tr>
-      <!--
-      <tr>
-        <th><?php echo $lang->product->desc;?></th>
-        <td colspan='2'><?php echo html::textarea('desc', '', "rows='2' class='form-control'");?></td>
-      </tr>
-      -->
-      <tr>
-        <th></th>
-        <td><?php echo html::submitButton();?></td>
-      </tr>
-    </table>
-  </form>
-</div>
+<form method='post' action='<?php echo inlink('create');?>' id='ajaxForm'>
+  <table class='table table-form'>
+    <tr>
+      <th class='w-50px'><?php echo $lang->product->name;?></th>
+      <td><?php echo html::input('name', '', "class='form-control'");?></td><td></td>
+    </tr>
+    <tr>
+      <th><?php echo $lang->product->type;?></th>
+      <td><?php echo html::select("type", $lang->product->typeList, '', "class='form-control'");?></td>
+    </tr>
+    <tr>
+      <th><?php echo $lang->product->status;?></th>
+      <td><?php echo html::select("status", $lang->product->statusList, '', "class='form-control'");?></td>
+    </tr>
+    <!--
+    <tr>
+      <th><?php echo $lang->product->desc;?></th>
+      <td colspan='2'><?php echo html::textarea('desc', '', "rows='2' class='form-control'");?></td>
+    </tr>
+    -->
+    <tr>
+      <th></th>
+      <td><?php echo html::submitButton();?></td>
+    </tr>
+  </table>
+</form>
 <?php include '../../../sys/common/view/footer.modal.html.php';?>
