d7cdd2914a389ccaaa98bacd0f90c3412796392a
daitingting <daitingting@cnezsoft.com>
2014-10-20 09:35:16 +0000

    * Adjust style for import file.
    git-svn-id: file:///tmp/ranzhi/trunk@1899 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/cash/trade/view/import.html.php
+++ app/cash/trade/view/import.html.php
@@ -11,26 +11,19 @@
  */
 ?>
 <?php include '../../../sys/common/view/header.modal.html.php';?>
-<div class='panel'>
-    <form method='post' id='ajaxForm' enctype='multipart/form-data' action='<?php echo inlink('import')?>'>
-    <table class='table table-form'>
-      <tr>
-        <th class='w-80px'><?php echo $lang->trade->depositor?></th>
-        <td class='w-p40'><?php echo html::select('depositor', $depositors, '', "class='form-control'")?></td>
-        <th class='w-60px'><?php echo $lang->trade->schema?></th>
-        <td><?php echo html::select('schema', $schemas, '', "class='form-control'")?></td>
-      </tr>
-      <tr>
-        <th><?php echo $lang->trade->importFile?></th>
-        <td><?php echo html::file('files', "class='form-control'")?></td>
-        <th><?php echo $lang->trade->encode?></th>
-        <td><?php echo html::select('encode', $lang->trade->encodeList, '', "class='form-control'")?></td>
-      </tr>
-      <tr>
-        <th></th>
-        <td colspan='3'><?php echo html::submitButton() . $lang->trade->fileNode;?></td>
-      </tr>
-    </table>
-  </form>
-</div>
+<form method='post' id='ajaxForm' enctype='multipart/form-data' action='<?php echo inlink('import')?>'>
+  <table class='table table-form table-condensed'>
+    <tr>
+      <th class='w-50px'><?php echo $lang->trade->depositor?></th>
+      <td class='w-180px'><?php echo html::select('depositor', $depositors, '', "class='form-control'")?></td>
+      <th class='w-50px'><?php echo $lang->trade->schema?></th>
+      <td class='w-180px'><?php echo html::select('schema', $schemas, '', "class='form-control'")?></td>
+      <th class='w-70px'><?php echo $lang->trade->importFile?></th>
+      <td class='w-200px'><?php echo html::file('files', "class='form-control'")?></td>
+      <th class='w-50px'><?php echo $lang->trade->encode?></th>
+      <td class='w-100px'><?php echo html::select('encode', $lang->trade->encodeList, '', "class='form-control'")?></td>
+      <th></th><td><?php echo html::submitButton() . $lang->trade->fileNode;?></td>
+    </tr>
+  </table>
+</form>
 <?php include '../../../sys/common/view/footer.modal.html.php';?>
