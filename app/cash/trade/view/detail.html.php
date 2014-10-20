3d8d4a7dcc14e15da15ef40de19bc273366e604e
daitingting <daitingting@cnezsoft.com>
2014-10-20 08:41:59 +0000

    * Add bordered for detail file.
    git-svn-id: file:///tmp/ranzhi/trunk@1897 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/cash/trade/view/detail.html.php
+++ app/cash/trade/view/detail.html.php
@@ -15,7 +15,7 @@
 <?php include '../../../sys/common/view/chosen.html.php';?>
 <?php js::set('key', count($details));?>
 <form method='post' id='ajaxForm' action='<?php echo inlink('detail', "tradeID={$trade->id}")?>' class='form-inline'>
-  <table class='table'>
+  <table class='table table-bordered'>
     <thead>
       <tr class='text-center'>
         <th class='w-100px'><?php echo $lang->trade->money;?></th>
@@ -48,7 +48,7 @@
   <tbody id='hiddenDetail'>  
     <tr>
       <td><?php echo html::input("money[key]", '', "class='form-control'")?></td>
-      <td><?php echo html::select("category[key][]", array('') + $categories, '', "class='form-control'")?></td>
+      <td><?php if(isset($categories)) echo html::select("category[key][]", array('') + $categories, '', "class='form-control'")?></td>
       <td><?php echo html::select("handlers[key][]", $users, $trade->handlers, "class='form-control' multiple")?></td>
       <td><?php echo html::textarea("desc[key]", '', "class='form-control'")?></td>
       <td>
