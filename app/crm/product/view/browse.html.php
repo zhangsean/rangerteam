3ffc4cb0bdf9c8e6a75ee0b381160d127847294e
daitingting <daitingting@cnezsoft.com>
2014-10-31 02:21:54 +0000

    * Adjust param for order of browse.
    git-svn-id: file:///tmp/ranzhi/trunk@2006 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/product/view/browse.html.php
+++ app/crm/product/view/browse.html.php
@@ -20,7 +20,7 @@
   <table class='table table-hover table-striped tablesorter table-data' id='productList'>
     <thead>
       <tr class='text-center'>
-        <?php $vars = "orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
+        <?php $vars = "mode={$mode}&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
         <th class='w-60px'> <?php commonModel::printOrderLink('id',          $orderBy, $vars, $lang->product->id);?></th>
         <th>                <?php commonModel::printOrderLink('name',        $orderBy, $vars, $lang->product->name);?></th>
         <th class='w-160px'><?php commonModel::printOrderLink('createdDate', $orderBy, $vars, $lang->product->createdDate);?></th>
