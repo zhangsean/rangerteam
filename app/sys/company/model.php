160942dfb1eb7db17b5cf289ac38fdec9b7248e2
daitingting <daitingting@cnezsoft.com>
2014-11-03 02:19:48 +0000

    * Add model file for ext.
    git-svn-id: file:///tmp/ranzhi/trunk@2009 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ app/sys/company/model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The model file of company of RanZhi.
+ *
+ * @copyright   Copyright 2013-2014 青岛易软天创网络科技有限公司(QingDao Nature Easy Soft Network Technology Co,LTD, www.cnezsoft.com)
+ * @license     LGPL
+ * @author      Tingting Dai <daitingting@xirangit.com>
+ * @package     company
+ * @version     $Id$
+ * @link        http://www.ranzhi.org
+ */
+class companyModel extends model
+{
+}
