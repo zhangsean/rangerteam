2922e535ccb657c87a4e01ea0b9176c8459a0fab
sunhao <sunhao@cnezsoft.com>
2014-12-11 11:34:25 +0000

    * fix highlight item on menu.
    git-svn-id: file:///tmp/ranzhi/trunk@2079 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/task/js/browse.js
+++ app/sys/task/js/browse.js
@@ -1,7 +1,7 @@
 $(function()
 {
-   if(v.mode)  $('.taskMenu').find('[href*=' + v.mode + ']').parent().addClass('active');
-   if(!v.mode) $('.taskMenu').find('ul li:nth-child(2) a').parent().addClass('active');
+   if(v.mode)  $('#menu').find('[href*=' + v.mode + ']').parent().addClass('active');
+   if(!v.mode) $('#menu').find('ul li:nth-child(2) a').parent().addClass('active');
 
     window.reloadDataTable = function()
     {
