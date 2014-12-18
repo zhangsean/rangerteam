dcca5a28776698e5a07d61bac2f8f1c4911f5561
daitingting <daitingting@cnezsoft.com>
2014-12-18 06:47:24 +0000

    * Adjust code.
    git-svn-id: file:///tmp/ranzhi/trunk@2135 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/package/config.php
+++ app/sys/package/config.php
@@ -1,4 +1,4 @@
 <?php
 $config->package = new stdclass();
-$config->package->apiRoot   = 'http://www.ranzhico.cn/extension-';
+$config->package->apiRoot   = 'http://www.ranzhico.com/extension-';
 $config->package->extPathes = array('module', 'bin', 'www', 'library', 'config');
