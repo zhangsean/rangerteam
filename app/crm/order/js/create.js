100da51f499075dd916685d78f0558fab648c896
daitingting <daitingting@cnezsoft.com>
2014-12-28 07:02:55 +0000

    * finish task #2906.
    git-svn-id: file:///tmp/ranzhi/trunk@2255 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ app/crm/order/js/create.js
@@ -0,0 +1,22 @@
+$(document).ready(function()
+{
+    /* Create customer when create an order. */
+    $('#createCustomer').change(function()
+    {
+        if($(this).prop('checked')) 
+        {
+            $('#customer').parents('td').find('.required').hide();
+            $(this).parents('.input-group').find('select').hide();
+            $('#customer_chosen').hide();
+            $(this).parents('.input-group').find('input[type=text][id=name]').show().focus();
+            $('.customerInfo').show();
+        }
+        else
+        {
+            $('#customer').parents('td').find('.required').show();
+            $('#customer_chosen').show();
+            $(this).parents('.input-group').find('input[type=text][id=name]').hide();
+            $('.customerInfo').hide();
+        }
+    })
+})
