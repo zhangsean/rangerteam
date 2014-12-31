6ab10bf3aa01816fdeefe4ca49426f612e89254a
sunhao <sunhao@cnezsoft.com>
2014-12-31 08:30:21 +0000

    * fix style for 'user/colleague'.
    git-svn-id: file:///tmp/ranzhi/trunk@2286 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/user/view/colleague.html.php
+++ app/sys/user/view/colleague.html.php
@@ -15,72 +15,68 @@ include $app->getModuleRoot() . 'common/view/header.html.php';
 include '../../common/view/treeview.html.php';
 js::set('deptID', $deptID);
 ?>
-<div class="row">
-  <div class='col-xs-2'>
-    <div class='panel'>
-      <div class='panel-heading'><strong><i class="icon-building"></i> <?php echo $lang->dept->common;?></strong></div>
-      <div class='panel-body'><div id='treeMenuBox'><?php echo $treeMenu;?></div></div>
-    </div>
+<div class='col-xs-2'>
+  <div class='panel'>
+    <div class='panel-heading'><strong><i class="icon-building"></i> <?php echo $lang->dept->common;?></strong></div>
+    <div class='panel-body'><div id='treeMenuBox'><?php echo $treeMenu;?></div></div>
   </div>
-  <div class='col-xs-10'>
-    <div class="row">
-      <div class='clearfix'>
-        <div class="panel">
-          <div class="panel-heading">
-            <strong><i class="icon-group"></i> <?php echo $lang->user->colleague;?></strong>
-            <div class="pull-right panel-actions search">
-              <form method='post' class='form-inline form-search w-300px'>
-                <div class="input-group">
-                  <?php echo html::input('query', $query, "class='form-control search-query' placeholder='{$lang->user->inputColleague}'"); ?>
-                  <span class="input-group-btn">
-                    <?php echo html::submitButton($lang->user->searchUser,"btn btn-primary"); ?>
-                  </span>
-                </div>
-              </form>
+</div>
+<div class='col-xs-10'>
+  <div class='clearfix'>
+    <div class="panel">
+      <div class="panel-heading">
+        <strong><i class="icon-group"></i> <?php echo $lang->user->colleague;?></strong>
+        <div class="pull-right panel-actions search">
+          <form method='post' class='form-inline form-search w-300px'>
+            <div class="input-group">
+              <?php echo html::input('query', $query, "class='form-control search-query' placeholder='{$lang->user->inputColleague}'"); ?>
+              <span class="input-group-btn">
+                <?php echo html::submitButton($lang->user->searchUser,"btn btn-primary"); ?>
+              </span>
             </div>
-          </div>
-          <div class='cards cards-user'>
-            <?php foreach($users as $user):?>
-            <div class='col-card'>
-              <div class='card card-user gender-<?php echo $user->gender?>'>
-                <div class='card-heading'>
-                  <?php if($user->avatar) echo html::image($user->avatar, "class='bg'");?>
-                  <div class='cover'></div>
-                  <div class='avatar'><?php if($user->avatar) echo html::image($user->avatar);?></div>
-                  <div class='title'>
-                    <h4 class='user-realname'><?php echo $user->realname;?></h4>
-                    <?php if($user->dept or $user->role):?>
-                    <div class='user-info'>
-                      <i class='icon-group'></i>
-                      <?php
-                      if($user->dept) echo zget($depts, $user->dept, ' ');
-                      if($user->role and $user->dept) echo '&nbsp;·&nbsp;';
-                      if($user->role) echo zget($lang->user->roleList, $user->role);
-                      ?>
-                    </div>
-                    <?php endif;?>
-                  </div>
-                  <div class='action'>
-                    <a href='javascript:;' class='btn-vcard'><i class='icon-qrcode'></i></a>
-                  </div>
+          </form>
+        </div>
+      </div>
+      <div class='cards cards-user'>
+        <?php foreach($users as $user):?>
+        <div class='col-card'>
+          <div class='card card-user gender-<?php echo $user->gender?>'>
+            <div class='card-heading'>
+              <?php if($user->avatar) echo html::image($user->avatar, "class='bg'");?>
+              <div class='cover'></div>
+              <div class='avatar'><?php if($user->avatar) echo html::image($user->avatar);?></div>
+              <div class='title'>
+                <h4 class='user-realname'><?php echo $user->realname;?></h4>
+                <?php if($user->dept or $user->role):?>
+                <div class='user-info'>
+                  <i class='icon-group'></i>
+                  <?php
+                  if($user->dept) echo zget($depts, $user->dept, ' ');
+                  if($user->role and $user->dept) echo '&nbsp;·&nbsp;';
+                  if($user->role) echo zget($lang->user->roleList, $user->role);
+                  ?>
                 </div>
-                <div class='card-content'>
-                  <dl class='contact-info'>
-                    <?php if($user->phone or $user->mobile) echo "<dd><i class='icon icon-phone-sign'></i> $user->phone $user->mobile</dd>";?>
-                    <?php if($user->qq) echo "<dd><i class='icon icon-qq'></i> " . html::a("http://wpa.qq.com/msgrd?v=3&uin={$user->qq}&site={$config->company->name}&menu=yes", $user->qq, "target='_blank'") . "</dd>";?>
-                    <?php if($user->email) echo "<dd><i class='icon icon-envelope-alt'></i> " . html::mailto($user->email, $user->email) . "</dd>";?>
-                    <?php if($user->address) echo "<dd><i class='icon icon-home'></i> $user->address </dd>";?>
-                  </dl>
-                </div>
-                <div class='vcard text-center'>
-                  <?php echo html::image(inlink('vcard', "user={$user->account}"));?>
-                </div>
+                <?php endif;?>
+              </div>
+              <div class='action'>
+                <a href='javascript:;' class='btn-vcard'><i class='icon-qrcode'></i></a>
               </div>
             </div>
-            <?php endforeach;?>
-            <?php $pager->show();?>
+            <div class='card-content'>
+              <dl class='contact-info'>
+                <?php if($user->phone or $user->mobile) echo "<dd><i class='icon icon-phone-sign'></i> $user->phone $user->mobile</dd>";?>
+                <?php if($user->qq) echo "<dd><i class='icon icon-qq'></i> " . html::a("http://wpa.qq.com/msgrd?v=3&uin={$user->qq}&site={$config->company->name}&menu=yes", $user->qq, "target='_blank'") . "</dd>";?>
+                <?php if($user->email) echo "<dd><i class='icon icon-envelope-alt'></i> " . html::mailto($user->email, $user->email) . "</dd>";?>
+                <?php if($user->address) echo "<dd><i class='icon icon-home'></i> $user->address </dd>";?>
+              </dl>
+            </div>
+            <div class='vcard text-center'>
+              <?php echo html::image(inlink('vcard', "user={$user->account}"));?>
+            </div>
           </div>
         </div>
+        <?php endforeach;?>
+        <?php $pager->show();?>
       </div>
     </div>
   </div>
