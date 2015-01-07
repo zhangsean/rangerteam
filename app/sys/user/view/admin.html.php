7a85ac4e9ab7222a30294f3b4221419127bbf931
daitingting <daitingting@cnezsoft.com>
2015-01-07 05:22:52 +0000

    * Format code.  * Adjust padding for col.
    git-svn-id: file:///tmp/ranzhi/trunk@2327 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/user/view/admin.html.php
+++ app/sys/user/view/admin.html.php
@@ -17,88 +17,90 @@ js::set('deptID', $deptID);
 js::set('from', 'admin');
 ?>
 <div class="col-md-12">
-  <?php include './deptside.html.php';?>
-  <div class='col-md-10'>
-    <div class="row">
-      <div class='clearfix'>
-        <div class="panel">
-          <div class="panel-heading">
-            <strong><i class="icon-group"></i> <?php echo $lang->user->list;?></strong>
-            <div class="pull-right panel-actions search">
-              <form method='post' class='form-inline form-search w-300px'>
-                <div class="input-group">
-                  <?php echo html::input('query', $query, "class='form-control search-query' placeholder='{$lang->user->inputUserName}'"); ?>
-                  <span class="input-group-btn">
-                    <?php echo html::submitButton($lang->user->searchUser,"btn btn-primary"); ?>
-                  </span>
-                </div>
-              </form>
+  <div class='row'> 
+    <?php include './deptside.html.php';?>
+    <div class='col-md-10'>
+      <div class="row">
+        <div class='clearfix'>
+          <div class="panel">
+            <div class="panel-heading">
+              <strong><i class="icon-group"></i> <?php echo $lang->user->list;?></strong>
+              <div class="pull-right panel-actions search">
+                <form method='post' class='form-inline form-search w-300px'>
+                  <div class="input-group">
+                    <?php echo html::input('query', $query, "class='form-control search-query' placeholder='{$lang->user->inputUserName}'"); ?>
+                    <span class="input-group-btn">
+                      <?php echo html::submitButton($lang->user->searchUser,"btn btn-primary"); ?>
+                    </span>
+                  </div>
+                </form>
+              </div>
             </div>
-          </div>
-          <table class='table table-hover table-striped table-bordered tablesorter table-fixed'>
-            <thead>
+            <table class='table table-hover table-striped table-bordered tablesorter table-fixed'>
+              <thead>
+                <tr class='text-center'>
+                  <?php $vars = "deptID=$deptID&query=$query&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
+                  <th><?php commonModel::printOrderLink('id',       $orderBy, $vars, $lang->user->id);?></th>
+                  <th><?php commonModel::printOrderLink('realname', $orderBy, $vars, $lang->user->realname);?></th>
+                  <th><?php commonModel::printOrderLink('account',  $orderBy, $vars, $lang->user->account);?></th>
+                  <th><?php commonModel::printOrderLink('gender',   $orderBy, $vars, $lang->user->gender);?></th>
+                  <th><?php commonModel::printOrderLink('dept',     $orderBy, $vars, $lang->user->dept);?></th>
+                  <th class='visible-lg'><?php commonModel::printOrderLink('join',     $orderBy, $vars, $lang->user->join);?></th>
+                  <th class='visible-lg'><?php commonModel::printOrderLink('visits',   $orderBy, $vars, $lang->user->visits);?></th>
+                  <th><?php commonModel::printOrderLink('last',     $orderBy, $vars, $lang->user->last);?></th>
+                  <th><?php commonModel::printOrderLink('ip',       $orderBy, $vars, $lang->user->ip);?></th>
+                  <th><?php commonModel::printOrderLink('locked',   $orderBy, $vars, $lang->user->status);?></th>
+                  <th><?php echo $lang->actions;?></th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php foreach($users as $user):?>
               <tr class='text-center'>
-                <?php $vars = "deptID=$deptID&query=$query&orderBy=%s&recTotal={$pager->recTotal}&recPerPage={$pager->recPerPage}&pageID={$pager->pageID}";?>
-                <th><?php commonModel::printOrderLink('id',       $orderBy, $vars, $lang->user->id);?></th>
-                <th><?php commonModel::printOrderLink('realname', $orderBy, $vars, $lang->user->realname);?></th>
-                <th><?php commonModel::printOrderLink('account',  $orderBy, $vars, $lang->user->account);?></th>
-                <th><?php commonModel::printOrderLink('gender',   $orderBy, $vars, $lang->user->gender);?></th>
-                <th><?php commonModel::printOrderLink('dept',     $orderBy, $vars, $lang->user->dept);?></th>
-                <th class='visible-lg'><?php commonModel::printOrderLink('join',     $orderBy, $vars, $lang->user->join);?></th>
-                <th class='visible-lg'><?php commonModel::printOrderLink('visits',   $orderBy, $vars, $lang->user->visits);?></th>
-                <th><?php commonModel::printOrderLink('last',     $orderBy, $vars, $lang->user->last);?></th>
-                <th><?php commonModel::printOrderLink('ip',       $orderBy, $vars, $lang->user->ip);?></th>
-                <th><?php commonModel::printOrderLink('locked',   $orderBy, $vars, $lang->user->status);?></th>
-                <th><?php echo $lang->actions;?></th>
+                <td><?php echo $user->id;?></td>
+                <td><?php echo $user->realname;?></td>
+                <td><?php echo $user->account;?></td>
+                <td><?php $gender = $user->gender; echo $lang->user->genderList->$gender;?></td>
+                <td><?php echo $depts[$user->dept];?></td>
+                <td class='visible-lg'><?php echo $user->join;?></td>
+                <td class='visible-lg'><?php echo $user->visits;?></td>
+                <td><?php echo $user->last;?></td>
+                <td><?php echo $user->ip;?></td>
+                <td>
+                  <?php
+                  $status = 'normal';
+                  if($user->fails > 4 and $user->locked > helper::now())
+                  {
+                      $status = 'locked';
+                      echo $lang->user->statusList->locked;
+                  }
+                  if($user->fails <= 4 and $user->locked > helper::now())
+                  {
+                      $status = 'forbidden';
+                      echo $lang->user->statusList->forbidden;
+                  }
+                  if($user->locked <= helper::now()) echo $lang->user->statusList->normal;
+                  ?>
+                </td>
+                <td class='operate'>
+                  <?php
+                  echo html::a($this->createLink('user', 'edit', "account=$user->account&from=admin"), $lang->edit);
+                  if($status == 'normal')
+                  {
+                      echo html::a($this->createLink('user', 'forbid', "userID=$user->id"), $lang->user->forbid, "class='forbider'");
+                  }
+                  else
+                  {
+                      echo html::a($this->createLink('user', 'active', "userID=$user->id"), $lang->user->active, "class='forbider'");
+                  }
+                  echo html::a($this->createLink('user', 'delete', "account=$user->account"), $lang->delete, "class='deleter'");
+                  ?>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-            <?php foreach($users as $user):?>
-            <tr class='text-center'>
-              <td><?php echo $user->id;?></td>
-              <td><?php echo $user->realname;?></td>
-              <td><?php echo $user->account;?></td>
-              <td><?php $gender = $user->gender; echo $lang->user->genderList->$gender;?></td>
-              <td><?php echo $depts[$user->dept];?></td>
-              <td class='visible-lg'><?php echo $user->join;?></td>
-              <td class='visible-lg'><?php echo $user->visits;?></td>
-              <td><?php echo $user->last;?></td>
-              <td><?php echo $user->ip;?></td>
-              <td>
-                <?php
-                $status = 'normal';
-                if($user->fails > 4 and $user->locked > helper::now())
-                {
-                    $status = 'locked';
-                    echo $lang->user->statusList->locked;
-                }
-                if($user->fails <= 4 and $user->locked > helper::now())
-                {
-                    $status = 'forbidden';
-                    echo $lang->user->statusList->forbidden;
-                }
-                if($user->locked <= helper::now()) echo $lang->user->statusList->normal;
-                ?>
-              </td>
-              <td class='operate'>
-                <?php
-                echo html::a($this->createLink('user', 'edit', "account=$user->account&from=admin"), $lang->edit);
-                if($status == 'normal')
-                {
-                    echo html::a($this->createLink('user', 'forbid', "userID=$user->id"), $lang->user->forbid, "class='forbider'");
-                }
-                else
-                {
-                    echo html::a($this->createLink('user', 'active', "userID=$user->id"), $lang->user->active, "class='forbider'");
-                }
-                echo html::a($this->createLink('user', 'delete', "account=$user->account"), $lang->delete, "class='deleter'");
-                ?>
-              </td>
-            </tr>
-            <?php endforeach;?>
-            </tbody>
-          </table>
-          <div class='table-footer'><?php $pager->show();?></div>
+              <?php endforeach;?>
+              </tbody>
+            </table>
+            <div class='table-footer'><?php $pager->show();?></div>
+          </div>
         </div>
       </div>
     </div>
