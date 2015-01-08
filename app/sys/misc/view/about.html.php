778898a5605773dd54c90480b249f3f2d016821f
chujilu <chujilu@cnezsoft.com>
2015-01-08 03:07:48 +0000

    * change about view.
    git-svn-id: file:///tmp/ranzhi/trunk@2352 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/misc/view/about.html.php
+++ app/sys/misc/view/about.html.php
@@ -2,19 +2,24 @@
 #aboutNav {margin: 20px -15px -15px; background-color: #fafafa; padding: 10px; border-top: 1px solid #e5e5e5}
 #aboutNav > .nav > li > a.text-important {color: #81511c}
 #aboutNav > .nav > li > a.text-important:hover {background-color: #81511c; color: #fff}
+#about {height: 200px; width:450px; margin:auto;}
 </style>
-<div class='text-center'>
-  <?php echo html::image($this->config->webRoot . 'theme/default/images/main/logo.png'); ?>
-  <h4><?php printf($lang->misc->version, $config->version);?></h4>
-  <div id='aboutNav' class='clearfix'>
-    <ul class='nav nav-pills pull-left'>
+<div id='about' class='text-center'>
+  <div class='col-md-4'>
+    <?php echo html::image($this->config->webRoot . 'theme/default/images/main/logo.login.png'); ?>
+    <h4><?php printf($lang->misc->version, $config->version);?></h4>
+  </div>
+  <div class='col-md-4'>
+    <ul class='nav nav-pills'>
+      <li><?php echo html::a('http://www.ranzhico.com/ranzhisaas-plans.html', "<i class='icon-cloud'></i> " . $lang->misc->yunranzhi, "target='_blank' class='text-important'")?></li>
       <li><?php echo html::a('http://www.ranzhico.com', "<i class='icon-globe'></i> " . $lang->misc->offcialSite, "target='_blank'")?></li>
       <li><?php echo html::a('https://www.ranzhico.com/page/support.html', "<i class='icon-question-sign'></i> " . $lang->misc->support, "target='_blank'")?></li>
+    </ul>
+  </div>
+  <div class='col-md-4'>
+    <ul class='nav nav-pills'>
       <li><?php echo html::a('https://www.ranzhico.com/book/', "<i class='icon-book'></i> " . $lang->misc->userbook, "target='_blank'")?></li>
       <li><?php echo html::a('https://www.ranzhico.com/forum/', "<i class='icon-comments-alt'></i> " . $lang->misc->forum, "target='_blank'")?></li>
-    </ul>
-    <ul class='nav nav-pills pull-right'>
-      <li><?php echo html::a('http://www.ranzhico.com/ranzhisaas-plans.html', "<i class='icon-cloud'></i> " . $lang->misc->yunranzhi, "target='_blank' class='text-important'")?></li>
     </ul>
   </div>
 </div>
