5060ed8cd36a56a9048e594c48b7bcc4ee79e25a
daitingting <daitingting@cnezsoft.com>
2015-01-12 07:40:40 +0000

    * Adjust code.
    git-svn-id: file:///tmp/ranzhi/trunk@2435 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/contact/view/block.html.php
+++ app/crm/contact/view/block.html.php
@@ -24,8 +24,9 @@
         <div class='row'>
         <div class='col-sm-10'>
         <div class='contact-info'>
+          <?php $companyName = isset($config->company->name) ? $config->company->name : '';?>
           <?php if($contact->phone or $contact->mobile) echo "<div><i class='icon-phone-sign'></i> $contact->phone $contact->mobile</div>";?>
-          <?php if($contact->qq) echo "<div class='f-14'><i class='icon-qq'></i> " . html::a("http://wpa.qq.com/msgrd?v=3&uin={$contact->qq}&site={$config->company->name}&menu=yes", $contact->qq, "target='_blank'") . "</div>";?>
+          <?php if($contact->qq) echo "<div class='f-14'><i class='icon-qq'></i> " . html::a("http://wpa.qq.com/msgrd?v=3&uin={$contact->qq}&site={$companyName}&menu=yes", $contact->qq, "target='_blank'") . "</div>";?>
           <?php if($contact->email) echo "<div class='f-14'><i class='icon-envelope-alt'></i> " . html::mailto($contact->email, $contact->email) . "</div>";?>
         </div>
         <p class='vcard text-center'><?php echo html::image(helper::createLink('contact', 'vcard', "contactID={$contact->id}"), "style='height:120px'");?></p>
