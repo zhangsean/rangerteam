70de1b5e93ce3450b1cbd67a55bee07cd16116b7
chujilu <chujilu@cnezsoft.com>
2015-01-22 01:23:42 +0000

    * Adjust code.
    git-svn-id: file:///tmp/ranzhi/trunk@2486 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/block/model.php
+++ app/sys/block/model.php
@@ -91,7 +91,6 @@ class blockModel extends model
             if(isset($parsedUrl['port'])) $link .= ':' . $parsedUrl['port']; 
             if(isset($parsedUrl['path'])) $link .= $parsedUrl['path']; 
             $link .= '?' . $parsedUrl['query'];
-            a($link);exit;
         }
 
         /* Send login request. */
