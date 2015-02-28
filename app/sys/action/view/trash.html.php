ab99c13045a76630086371f157418714c2c4c21e
chujilu <chujilu@cnezsoft.com>
2015-02-28 04:16:31 +0000

    * Fix a bug.
    git-svn-id: file:///tmp/ranzhi/trunk@2552 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/action/view/trash.html.php
+++ app/sys/action/view/trash.html.php
@@ -42,8 +42,8 @@
         <td><?php echo $action->date;?></td>
         <td>
           <?php
-          commonModel::printLink('action', 'undelete', "actionid=$action->id", $lang->action->undelete, 'hiddenwin');
-          if($type == 'all') commonModel::printLink('action', 'hideOne',  "actionid=$action->id", $lang->action->hideOne, 'hiddenwin');
+          commonModel::printLink('action', 'undelete', "actionid=$action->id", $lang->action->undelete, '', "class='ajax'");
+          if($type == 'all') commonModel::printLink('action', 'hideOne',  "actionid=$action->id", $lang->action->hideOne, '', "class='ajax'");
           ?>
         </td>
       </tr>
