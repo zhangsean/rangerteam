6e628dc00d86ec76f2fb12486a8586fac41e7902
daitingting <daitingting@cnezsoft.com>
2015-03-02 08:14:29 +0000

    * Adjust type for fee.  * Add inveset and redeem for type.
    git-svn-id: file:///tmp/ranzhi/trunk@2569 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/upgrade2.0.sql
+++ db/upgrade2.0.sql
@@ -2,3 +2,7 @@ ALTER TABLE `oa_doc` CHANGE `lib` `lib` mediumint(8) unsigned NOT NULL AFTER `pr
 CHANGE `module` `module` mediumint(8) unsigned NOT NULL AFTER `lib`;
 
 ALTER TABLE `oa_relation` RENAME TO `sys_relation`;
+
+UPDATE `cash_trade` SET `type` = 'out' WHERE `type`='fee';
+
+ALTER TABLE `cash_trade` CHANGE `type` `type` enum('in','out','transferin','transferout','inveset','redeem') NOT NULL AFTER `dept`;
