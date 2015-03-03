9861d149f22f76fa8dc18fe24388bec5832cc85d
daitingting <daitingting@cnezsoft.com>
2015-03-03 00:22:38 +0000

    * Set account as realname if realname is empty.
    git-svn-id: file:///tmp/ranzhi/trunk@2575 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/project/model.php
+++ app/oa/project/model.php
@@ -58,6 +58,7 @@ class projectModel extends model
     {
         $members = $this->dao->select('account')->from(TABLE_TEAM)->where('type')->eq('project')->andWhere('id')->eq($projectID)->fetchPairs('account');
         $users = $this->dao->select('account, realname')->from(TABLE_USER)->where('account')->in($members)->orderBy('id_asc')->fetchPairs();
+        foreach($users as $account => $realname) if($realname == '') $users[$account] = $account; 
         return array('' => '') + $users;
     }
 
