c4e950fc979926372c270ec921f8fed086a54400
daitingting <daitingting@cnezsoft.com>
2015-03-23 09:34:37 +0000

    * Adjust width for showImport page.
    git-svn-id: file:///tmp/ranzhi/trunk@2674 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/cash/trade/view/showimport.html.php
+++ app/cash/trade/view/showimport.html.php
@@ -20,13 +20,13 @@
       <thead>
         <tr class='text-center'>
           <th class='w-100px'><?php echo $lang->trade->depositor;?></th>
-          <th class='w-100px'><?php echo $lang->trade->type;?></th> 
+          <th class='w-90px'><?php echo $lang->trade->type;?></th> 
           <th class='w-120px'><?php echo $lang->trade->category;?></th> 
-          <th class='w-180px'><?php echo $lang->trade->trader;?></th> 
-          <th class='w-120px'><?php echo $lang->trade->money;?></th>
+          <th class='w-160px'><?php echo $lang->trade->trader;?></th> 
+          <th class='w-100px'><?php echo $lang->trade->money;?></th>
           <th class='w-80px'><?php echo $lang->trade->dept;?></th>
-          <th class='w-200px'><?php echo $lang->trade->handlers;?></th>
-          <th class='w-120px'><?php echo $lang->trade->date;?></th>
+          <th class='w-170px'><?php echo $lang->trade->handlers;?></th>
+          <th class='w-110px'><?php echo $lang->trade->date;?></th>
           <th><?php echo $lang->trade->desc;?></th>
         </tr>
       </thead>
