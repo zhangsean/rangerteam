12d67d926317794978bb8c6116436713e527cb4e
daitingting <daitingting@cnezsoft.com>
2015-04-08 08:38:25 +0000

    * Format code.
    git-svn-id: file:///tmp/ranzhi/trunk@2684 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/misc/lang/en.php
+++ app/sys/misc/lang/en.php
@@ -11,8 +11,8 @@
  */
 
 if(!isset($lang->misc)) $lang->misc = new stdclass();
-$lang->misc->version           = 'Version %s';
-$lang->misc->offcialSite       = 'Official';
-$lang->misc->support           = 'Support';
-$lang->misc->userbook          = 'Manual';
-$lang->misc->forum             = 'Forum';
+$lang->misc->version     = 'Version %s';
+$lang->misc->offcialSite = 'Official';
+$lang->misc->support     = 'Support';
+$lang->misc->userbook    = 'Manual';
+$lang->misc->forum       = 'Forum';
--- app/sys/misc/lang/zh-cn.php
+++ app/sys/misc/lang/zh-cn.php
@@ -11,8 +11,8 @@
  */
 
 if(!isset($lang->misc)) $lang->misc = new stdclass();
-$lang->misc->version           = '版本%s';
-$lang->misc->offcialSite       = '官方网站';
-$lang->misc->support           = '技术支持';
-$lang->misc->userbook          = '用户手册';
-$lang->misc->forum             = '论坛交流';
+$lang->misc->version     = '版本%s';
+$lang->misc->offcialSite = '官方网站';
+$lang->misc->support     = '技术支持';
+$lang->misc->userbook    = '用户手册';
+$lang->misc->forum       = '论坛交流';
