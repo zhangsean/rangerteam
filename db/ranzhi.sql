4591ada48172c669c9d7725e0abd3e93df35887c
daitingting <daitingting@cnezsoft.com>
2015-04-20 05:23:13 +0000

    * Add field for category.
    git-svn-id: file:///tmp/ranzhi/trunk@2762 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/ranzhi.sql
+++ db/ranzhi.sql
@@ -432,6 +432,7 @@ CREATE TABLE `sys_category` (
   `postedDate` datetime NOT NULL,
   `postID` mediumint(9) NOT NULL,
   `replyID` mediumint(8) unsigned NOT NULL,
+  `rights` char(30) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tree` (`type`),
   KEY `order` (`order`),
