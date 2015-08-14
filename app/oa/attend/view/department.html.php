bd0ffc76235eb67bbe5fb47f30e8f0962d641a96
chujilu <chujilu@cnezsoft.com>
2015-08-14 09:37:26 +0000

    * Add month slide.
    git-svn-id: file:///tmp/ranzhi/trunk@2901 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/attend/view/department.html.php
+++ app/oa/attend/view/department.html.php
@@ -11,38 +11,64 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<table class='table table-data table-bordered text-center'>
-  <thead>
-    <tr><th class='text-center' colspan="<?php echo $dayNum + 3?>"><?php echo $currentYear . $lang->year . $currentMonth . $lang->month . $lang->attend->report;?></th></tr>
-  </thead>
-  <tr>
-    <th rowspan='2'><?php echo $lang->user->id;?></th>
-    <th rowspan='2'><?php echo $lang->user->dept;?></th>
-    <th rowspan='2'><?php echo $lang->user->realname;?></th>
-    <?php for($day = 1; $day <= $dayNum; $day++):?>
-    <th><?php echo $day?></th>
-    <?php endfor;?>
-  </tr>
-  <tr>
-    <?php $weekOffset = date('w', strtotime("$currentYear-$currentMonth-01")) - 1;?>
-    <?php for($day = 1; $day <= $dayNum; $day++):?>
-    <th><?php echo $lang->datepicker->dayNames[($day + $weekOffset) % 7]?></th>
-    <?php endfor;?>
-  </tr>
-  <?php foreach($attends as $account => $userAttends):?>
-  <tr>
-    <td><?php echo isset($users[$account]) ? $users[$account]->id : '';?></td>
-    <td><?php echo $deptList[$currentDept]->name?></td>
-    <td><?php echo isset($users[$account]) ? $users[$account]->realname : '';?></td>
-    <?php for($day = 1; $day <= $dayNum; $day++):?>
-    <?php $currentDate = date("Y-m-d", strtotime("{$currentYear}-{$currentMonth}-{$day}"));?>
-    <td>
-      <?php if(isset($userAttends[$currentDate])):?>
-      <span class='attend-<?php echo $userAttends[$currentDate]->status?>'><?php echo $userAttends[$currentDate]->status?></span>
-      <?php endif;?>
-    </td>
-    <?php endfor;?>
-  </tr>
-  <?php endforeach;?>
-</table>
+<div class='row'>
+  <div class='col-xs-2'>
+    <div class='panel panel-sm'>
+      <div class='panel-body'>
+        <ul class='tree' data-collapsed='true'>
+          <?php foreach($yearList as $year):?>
+          <li>
+            <?php commonModel::printLink('attend', 'personal', "date=$year", $year);?>
+            <ul>
+              <?php foreach($monthList[$year] as $month):?>
+              <li><?php commonModel::printLink('attend', 'personal', "date=$year$month", $year . $month);?></li>
+              <?php endforeach;?>
+            </ul>
+          </li>
+          <?php endforeach;?>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class='col-xs-10'>
+    <div class='panel'>
+      <div class='panel-heading text-center'>
+        <strong><?php echo $currentYear . $lang->year . $currentMonth . $lang->month . $lang->attend->report;?></strong>
+      </div>
+      <table class='table table-data table-bordered text-center table-fixed'>
+        <thead>
+          <tr class='text-center'>
+            <th rowspan='2' class='w-40px valign-middle'><?php echo $lang->user->id;?></th>
+            <th rowspan='2' class='w-80px valign-middle'><?php echo $lang->user->dept;?></th>
+            <th rowspan='2' class='w-80px valign-middle'><?php echo $lang->user->realname;?></th>
+            <?php for($day = 1; $day <= $dayNum; $day++):?>
+            <th><?php echo $day?></th>
+            <?php endfor;?>
+          </tr>
+          <tr class='text-center'>
+            <?php $weekOffset = date('w', strtotime("$currentYear-$currentMonth-01")) - 1;?>
+            <?php for($day = 1; $day <= $dayNum; $day++):?>
+            <th><?php echo $lang->datepicker->dayNames[($day + $weekOffset) % 7]?></th>
+            <?php endfor;?>
+          </tr>
+        </thead>
+        <?php foreach($attends as $account => $userAttends):?>
+        <tr>
+          <td><?php echo isset($users[$account]) ? $users[$account]->id : '';?></td>
+          <td><?php echo $deptList[$currentDept]->name?></td>
+          <td><?php echo isset($users[$account]) ? $users[$account]->realname : '';?></td>
+          <?php for($day = 1; $day <= $dayNum; $day++):?>
+          <?php $currentDate = date("Y-m-d", strtotime("{$currentYear}-{$currentMonth}-{$day}"));?>
+          <td>
+            <?php if(isset($userAttends[$currentDate])):?>
+            <span class='attend-<?php echo $userAttends[$currentDate]->status?>'><?php echo $userAttends[$currentDate]->status?></span>
+            <?php endif;?>
+          </td>
+          <?php endfor;?>
+        </tr>
+        <?php endforeach;?>
+      </table>
+    </div>
+  </div>
+</div>
 <?php include '../../common/view/footer.html.php';?>
--- app/oa/attend/view/personal.html.php
+++ app/oa/attend/view/personal.html.php
@@ -11,72 +11,75 @@
  */
 ?>
 <?php include '../../common/view/header.html.php';?>
-<div id='menuActions'>
-    <div class='date dropdown'>
-      <button type='button' class='btn btn-sm btn-default dropdown-toggle' data-toggle='dropdown'><?php echo $currentYear . $lang->year . $currentMonth . $lang->month;?> <span class="caret"></span></button>
-      <ul class='dropdown-menu pull-right'>
-        <?php foreach($yearList as $year):?>
-        <li class='dropdown-submenu'>
-          <?php echo html::a("javascript:void(0)", $year);?>
-          <ul class='dropdown-menu'>
-            <?php foreach($monthList[$year] as $month):?>
-            <li><?php echo html::a(helper::createLink('attend', 'personal', "date=$year$month"), $month . $lang->month);?></li>
-            <?php endforeach;?>
-          </ul>
-        </li>
-        <?php endforeach;?>
-      </ul>
-    </div>
-
-</div>
 <div class='row'>
-  <?php $weekOffset = date('w', strtotime("$currentYear-$currentMonth-01")) - 1;?>
-  <?php for($weekIndex = 0; $weekIndex < $weekNum; $weekIndex++):?>
-  <div class='col-xs-3'>
-    <div class='panel'>
-      <div class='panel-heading'>
-        <strong><?php echo $lang->attend->weeks[$weekIndex];?></strong>
-      </div>
-      <div class='panel-body no-padding'>
-        <table class='table table-data table-fixed text-center'>
-          <tr>
-            <th><?php echo $lang->attend->date;?></th>
-            <th class='text-center'><?php echo $lang->attend->dayName;?></th>
-            <th class='text-center'><?php echo $lang->attend->signIn;?></th>
-            <th class='text-center'><?php echo $lang->attend->signOut;?></th>
-          </tr>
-          <?php $startDay = $weekIndex * 7 + 1;?>
-          <?php for($day = $startDay; $day <= $dayNum and $day < $startDay + 7; $day++):?>
-            <?php 
-              $currentDate = date("Y-m-d", strtotime("{$currentYear}-{$currentMonth}-{$day}"));
-              $status = isset($attends[$currentDate]) ? $attends[$currentDate]->status : '';
-            ?>
-            <tr class="attend-<?php echo $status?>">
-              <td><?php echo $currentDate;?></td>
-              <td><?php echo $lang->datepicker->dayNames[($day + $weekOffset) % 7]?></td>
-              <?php if(isset($attends[$currentDate])):?>
-              <?php $url = $this->createLink('oa.attend', 'edit', "date=" . str_replace('-', '', $currentDate));?>
-              <td class='attend-signin'>
-                <?php $signIn = substr($attends[$currentDate]->signIn, 0, 5);?>
-                <?php if(strpos(',late,both,absent,', $status) !== false) $signIn = html::a($url, $signIn . " <i class='icon icon-edit'></i>" , "data-toggle='modal' data-type='iframe' data-title='{$lang->attend->manual}'");?>
-                <?php echo $signIn;?>
-              </td>
-              <td class='attend-signout'>
-                <?php $signOut = substr($attends[$currentDate]->signOut, 0, 5);?>
-                <?php if(strpos(',early,both,absent,', $status) !== false) $signOut = html::a($url, $signOut . " <i class='icon icon-edit'></i>" , "data-toggle='modal' data-type='iframe' data-title='{$lang->attend->manual}'");?>
-                <?php echo $signOut;?>
-              </td>
-              <?php else:?>
-              <td></td>
-              <td></td>
-              <?php endif;?>
-            </tr>
-          <?php endfor;?>
-        </table>
+  <div class='col-xs-2'>
+    <div class='panel panel-sm'>
+      <div class='panel-body'>
+        <ul class='tree' data-collapsed='true'>
+          <?php foreach($yearList as $year):?>
+          <li>
+            <?php commonModel::printLink('attend', 'personal', "date=$year", $year);?>
+            <ul>
+              <?php foreach($monthList[$year] as $month):?>
+              <li><?php commonModel::printLink('attend', 'personal', "date=$year$month", $year . $month);?></li>
+              <?php endforeach;?>
+            </ul>
+          </li>
+          <?php endforeach;?>
+        </ul>
       </div>
     </div>
   </div>
-  <?php endfor;?>
+  <div class='col-xs-10'>
+    <div class='row'>
+      <?php $weekOffset = date('w', strtotime("$currentYear-$currentMonth-01")) - 1;?>
+      <?php for($weekIndex = 0; $weekIndex < $weekNum; $weekIndex++):?>
+      <div class='col-xs-4'>
+        <div class='panel'>
+          <div class='panel-heading'>
+            <strong><?php echo $lang->attend->weeks[$weekIndex];?></strong>
+          </div>
+          <div class='panel-body no-padding'>
+            <table class='table table-data table-fixed text-center'>
+              <tr>
+                <th><?php echo $lang->attend->date;?></th>
+                <th class='text-center'><?php echo $lang->attend->dayName;?></th>
+                <th class='text-center'><?php echo $lang->attend->signIn;?></th>
+                <th class='text-center'><?php echo $lang->attend->signOut;?></th>
+              </tr>
+              <?php $startDay = $weekIndex * 7 + 1;?>
+              <?php for($day = $startDay; $day <= $dayNum and $day < $startDay + 7; $day++):?>
+                <?php 
+                  $currentDate = date("Y-m-d", strtotime("{$currentYear}-{$currentMonth}-{$day}"));
+                  $status = isset($attends[$currentDate]) ? $attends[$currentDate]->status : '';
+                ?>
+                <tr class="attend-<?php echo $status?>">
+                  <td><?php echo $currentDate;?></td>
+                  <td><?php echo $lang->datepicker->dayNames[($day + $weekOffset) % 7]?></td>
+                  <?php if(isset($attends[$currentDate])):?>
+                  <?php $url = $this->createLink('oa.attend', 'edit', "date=" . str_replace('-', '', $currentDate));?>
+                  <td class='attend-signin'>
+                    <?php $signIn = substr($attends[$currentDate]->signIn, 0, 5);?>
+                    <?php if(strpos(',late,both,absent,', $status) !== false) $signIn = html::a($url, $signIn . " <i class='icon icon-edit'></i>" , "data-toggle='modal' data-type='iframe' data-title='{$lang->attend->manual}'");?>
+                    <?php echo $signIn;?>
+                  </td>
+                  <td class='attend-signout'>
+                    <?php $signOut = substr($attends[$currentDate]->signOut, 0, 5);?>
+                    <?php if(strpos(',early,both,absent,', $status) !== false) $signOut = html::a($url, $signOut . " <i class='icon icon-edit'></i>" , "data-toggle='modal' data-type='iframe' data-title='{$lang->attend->manual}'");?>
+                    <?php echo $signOut;?>
+                  </td>
+                  <?php else:?>
+                  <td></td>
+                  <td></td>
+                  <?php endif;?>
+                </tr>
+              <?php endfor;?>
+            </table>
+          </div>
+        </div>
+      </div>
+      <?php endfor;?>
+    </div>
+  </div>
 </div>
 <?php include '../../common/view/footer.html.php';?>
-
