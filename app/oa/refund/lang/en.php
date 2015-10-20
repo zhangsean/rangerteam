1616307d383624c7661cff877172060b41147faf
chujilu <chujilu@cnezsoft.com>
2015-10-20 02:58:30 +0000

    * Add lang file of refund.
    git-svn-id: file:///tmp/ranzhi/trunk@3003 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

---
+++ app/oa/refund/lang/en.php
@@ -0,0 +1,7 @@
+<?php
+if(!isset($lang->refund)) $lang->refund = new stdclass();
+$lang->refund->common   = 'Reimbursement';
+$lang->refund->create   = 'Create';
+$lang->refund->browse   = 'Browse List';
+$lang->refund->edit     = 'Edit reimbursement';
+$lang->refund->settings = 'Settings';
---
+++ app/oa/refund/lang/zh-cn.php
@@ -0,0 +1,7 @@
+<?php
+if(!isset($lang->refund)) $lang->refund = new stdclass();
+$lang->refund->common   = '报销';
+$lang->refund->create   = '申请报销';
+$lang->refund->browse   = '报销列表';
+$lang->refund->edit     = '编辑报销';
+$lang->refund->settings = '设置';
