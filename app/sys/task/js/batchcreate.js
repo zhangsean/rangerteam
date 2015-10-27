934414e851b2dd711235b457fcd5fcaaf5402744
daitingting <daitingting@cnezsoft.com>
2015-10-27 01:12:30 +0000

    * Estimate is readonly if task is multiple.
    git-svn-id: file:///tmp/ranzhi/trunk@3062 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/task/js/batchcreate.js
+++ app/sys/task/js/batchcreate.js
@@ -9,11 +9,13 @@ $(document).ready(function()
         {
             checkboxObj.parents('td').next('td').find('select').addClass('hidden');
             checkboxObj.parents('td').next('td').find('a').removeClass('hidden');
+            checkboxObj.parents('tr').find('[id*=estimate]').attr('readonly', true);
         }
         else
         {
             checkboxObj.parents('td').next('td').find('select').removeClass('hidden');
             checkboxObj.parents('td').next('td').find('a').addClass('hidden');
+            checkboxObj.parents('tr').find('[id*=estimate]').attr('readonly', false);
         }
     });
 });
