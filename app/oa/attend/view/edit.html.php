75adc9ba21897a3dc35b659e0c381862e2da7c91
daitingting <daitingting@cnezsoft.com>
2015-10-28 08:16:16 +0000

    * Adjust style.
    git-svn-id: file:///tmp/ranzhi/trunk@3086 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/attend/view/edit.html.php
+++ app/oa/attend/view/edit.html.php
@@ -12,63 +12,57 @@
 ?>
 <?php include '../../../sys/common/view/header.modal.html.php';?>
 <?php include '../../../sys/common/view/datepicker.html.php';?>
-<?php js::set('reason', $attend->reason);?>
 <?php js::set('status', $attend->status);?>
+<?php js::set('reason', $attend->reason);?>
 <div class='panel-body'>
   <form id='ajaxForm' method='post' action='<?php echo $this->createLink('attend', 'edit', "date=$date")?>'>
     <table class='table table-form table-condensed'>
-      <tr>
-        <th class='w-80px'><?php echo $lang->attend->date?></th>
-        <td class='w-p40'><?php echo $attend->dayName;?></td>
-        <th><?php echo $lang->attend->status?></th>
-        <td><?php echo zget($lang->attend->statusList, $attend->status)?></td>
-      </tr> 
       <?php if(!empty($attend->reviewStatus)):?>
       <tr>
         <th><?php echo $lang->attend->reviewStatus?></th>
-        <td colspan='3'><?php echo zget($lang->attend->reviewStatusList, $attend->reviewStatus) . " " . $attend->reviewedBy . " " . $attend->reviewedDate?></td>
+        <td><?php echo zget($lang->attend->reviewStatusList, $attend->reviewStatus) . " " . $attend->reviewedBy . " " . $attend->reviewedDate?></td>
       </tr>
       <?php endif;?>
+      <tr>
+        <th class='w-80px'><?php echo $lang->attend->date?></th>
+        <td><?php echo $attend->dayName;?></td>
+      </tr> 
     </table>
     <table class='table table-form table-condensed editMode'>
       <?php if(strpos(',late,both,absent', $attend->status) !== false):?>
       <tr id='trIn'>
-        <th><?php echo $lang->attend->manualIn?></th>
-        <td colspan='3'><?php echo html::input('manualIn', empty($attend->manualIn) ? $this->config->attend->signInLimit : $attend->manualIn, "class='form-control form-time'")?></td>
+        <th class='w-80px'><?php echo $lang->attend->manualIn?></th>
+        <td><?php echo html::input('manualIn', empty($attend->manualIn) ? $this->config->attend->signInLimit : $attend->manualIn, "class='form-control form-time'")?></td>
       </tr>
       <?php endif;?>
       <?php if(strpos(',early,both,absent', $attend->status) !== false):?>
       <tr id='trOut'>
         <th><?php echo $lang->attend->manualOut?></th>
-        <td colspan='3'><?php echo html::input('manualOut', empty($attend->manualOut) ? $this->config->attend->signOutLimit : $attend->manualOut, "class='form-control form-time'")?></td>
+        <td><?php echo html::input('manualOut', empty($attend->manualOut) ? $this->config->attend->signOutLimit : $attend->manualOut, "class='form-control form-time'")?></td>
       </tr> 
       <?php endif;?>
       <tr>
         <th><?php echo $lang->attend->desc?></th>
-        <td colspan='3'><?php echo html::textarea('desc', $attend->desc, "class='form-control'")?></td>
+        <td><?php echo html::textarea('desc', $attend->desc, "class='form-control'")?></td>
       </tr> 
-      <tr><th></th><td clospan='3'><?php echo html::submitButton();?></td></tr>
+      <tr><th></th><td><?php echo html::submitButton();?></td></tr>
     </table>
     <table class='table table-form table-condensed viewMode'>
-      <tr>
-        <th class='w-80px'><?php echo $lang->attend->reason?></th>
-        <td colspan='3'><?php echo zget($lang->attend->reasonList, $attend->reason, $attend->reason);?></td>
-      </tr> 
       <?php if(strpos(',late,both,absent', $attend->status) !== false):?>
       <tr id='trIn'>
-        <th><?php echo $lang->attend->manualIn?></th>
-        <td colspan='3'><?php echo $attend->manualIn;?></td>
+        <th class='w-80px'><?php echo $lang->attend->manualIn?></th>
+        <td><?php echo $attend->manualIn;?></td>
       </tr>
       <?php endif;?>
       <?php if(strpos(',early,both,absent', $attend->status) !== false):?>
       <tr id='trOut'>
         <th><?php echo $lang->attend->manualOut?></th>
-        <td colspan='3'><?php echo $attend->manualOut;?></td>
+        <td><?php echo $attend->manualOut;?></td>
       </tr> 
       <?php endif;?>
       <tr>
         <th><?php echo $lang->attend->desc?></th>
-        <td colspan='3'><?php echo $attend->desc;?></td>
+        <td><?php echo $attend->desc;?></td>
       </tr> 
       <?php if($attend->reviewStatus != 'pass'):?>
       <tr><th></th><td clospan='3'><?php echo html::commonButton($lang->edit, "btn btn-primary edit");?></td></tr>
