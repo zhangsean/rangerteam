e0183e09abb628111b212b28426cbf7d72f4a7e1
daitingting <daitingting@cnezsoft.com>
2015-10-29 02:07:11 +0000

    * Fix a bug.
    git-svn-id: file:///tmp/ranzhi/trunk@3099 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/common/view/selectlang.html.php
+++ app/sys/common/view/selectlang.html.php
@@ -11,7 +11,7 @@
  */
 ?>
 <?php $clientLang = $this->app->getClientLang();?>
-<a href='###' tabindex='-1'><i class='icon icon-globe'></i> <?php echo $config->langs[$clientLang]?></a>
+<a href='###' data-toggle='dropdown' class='dropdown-toggle'><i class='icon icon-globe'></i> <?php echo $config->langs[$clientLang]?></a>
 <ul class='dropdown-menu'>
   <?php
   $langs = $config->langs;
