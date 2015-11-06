262213881d15223df2d94404198368d2f21653ea
daitingting <daitingting@cnezsoft.com>
2015-11-06 01:21:48 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3132 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/attend/lang/en.php
+++ app/oa/attend/lang/en.php
@@ -91,8 +91,8 @@ $lang->attend->mustSignOutList['no']  = 'not need';
 
 $lang->attend->weeks = array('First week', 'Second week', 'Third week', 'Fourth week', 'Fifth week', 'Sixth week');
 
-$lang->attend->notice['today']    = "Your yestoday's attendance is %s, <a href='%s' %s>Go to edit.</a>";
-$lang->attend->notice['yestoday'] = "Your today's attendance is %s, <a href='%s' %s>Go to edit.</a>";
+$lang->attend->notice['today']    = "<p>Your yestoday's attendance is %s, <a href='%s' %s>Go to edit.</a></p>";
+$lang->attend->notice['yestoday'] = "<p>Your today's attendance is %s, <a href='%s' %s>Go to edit.</a></p>";
 $lang->attend->notice['absent']   = "No record";
 
 $lang->attend->confirmReview['pass']   = 'Are sure to pass it?';
