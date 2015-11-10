d32b986ff1bdce85d3d775fc2c950b25ceb2fbe8
chujilu <chujilu@cnezsoft.com>
2015-11-10 09:17:09 +0000

    + Add task list of dashboard module.
    git-svn-id: file:///tmp/ranzhi/trunk@3141 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/common/lang/zh-cn.php
+++ app/oa/common/lang/zh-cn.php
@@ -24,6 +24,10 @@ $lang->menu->oa->trip      = '出差|trip|personal|';
 $lang->menu->oa->refund    = '报销|refund|personal|';
 
 $lang->dashboard = new stdclass();
+$lang->dashboard->menu = new stdclass();
+$lang->dashboard->menu->index = '首页|dashboard|index|';
+$lang->dashboard->menu->todo  = '待办|dashboard|todo|';
+$lang->dashboard->menu->task  = '任务|dashboard|task|';
 
 $lang->todo = new stdclass();
 $lang->todo->menu = new stdclass();
