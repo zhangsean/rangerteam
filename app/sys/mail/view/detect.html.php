6f95afbfa3ba6e82801e39a92a8ee206af51d6d1
daitingting <daitingting@cnezsoft.com>
2015-11-12 09:32:37 +0000

    * Adjust style for tip of mail.
    git-svn-id: file:///tmp/ranzhi/trunk@3159 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/mail/view/detect.html.php
+++ app/sys/mail/view/detect.html.php
@@ -16,11 +16,11 @@
   <div class='panel-body'>
     <form method='post' id='ajaxForm'>
       <div class='form-group'><label for='fromAddress' class='col-sm-12'><?php echo $lang->mail->inputFromEmail; ?></label></div>
-      <div class='form-group'>
-        <div class='col-xs-10 col-sm-6 col-md-3'><?php echo html::input('fromAddress', $fromAddress, "class='form-control'"); ?></div>
-        <div class='col-xs-2 col-sm-6 col-md-3'>
-          <?php echo html::submitButton($lang->mail->nextStep); ?>
-          &nbsp;&nbsp;<i class='icon-question-sign' data-toggle='tooltip' data-original-title='<?php echo $lang->mail->openTip?>'></i>
+      <div class='form-group row'>
+        <div class='col-xs-6 col-sm-6 col-md-3'><?php echo html::input('fromAddress', $fromAddress, "class='form-control'"); ?></div>
+        <div class='col-xs-6 col-sm-6 col-md-9'>
+          <?php echo html::submitButton($lang->mail->nextStep);?>
+          <span><strong class='text-info'> <?php echo $lang->mail->openTip;?></strong></span>
         </div>
       </div>
     </form>
