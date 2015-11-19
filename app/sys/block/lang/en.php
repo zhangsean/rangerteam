da4091a018fa8b1811a720149eca2a22dbefbc2c
daitingting <daitingting@cnezsoft.com>
2015-11-19 05:38:48 +0000

    * Set default block in dashboard.
    git-svn-id: file:///tmp/ranzhi/trunk@3193 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/block/lang/en.php
+++ app/sys/block/lang/en.php
@@ -30,28 +30,32 @@ $lang->block->editBlock          = 'Edit Block';
 $lang->block->ordersSaved        = 'Sort have been saved';
 $lang->block->confirmRemoveBlock = 'Are you sure remove block [{0}] ?';
 
-$lang->block->default['oa']['1']['title'] = 'System Announcement';
-$lang->block->default['oa']['1']['block'] = 'announce';
-$lang->block->default['oa']['1']['grid']  = 4;
+$lang->block->default['oa']['1']['title'] = 'Calendar';
+$lang->block->default['oa']['1']['block'] = 'attend';
+$lang->block->default['oa']['1']['grid']  = 12;
 
-$lang->block->default['oa']['1']['params']['num'] = 15;
-
-$lang->block->default['oa']['2']['title'] = 'The task of assigned to me';
-$lang->block->default['oa']['2']['block'] = 'task';
+$lang->block->default['oa']['2']['title'] = 'System Announcement';
+$lang->block->default['oa']['2']['block'] = 'announce';
 $lang->block->default['oa']['2']['grid']  = 4;
 
-$lang->block->default['oa']['2']['params']['num']     = 15;
-$lang->block->default['oa']['2']['params']['orderBy'] = 'id_desc';
-$lang->block->default['oa']['2']['params']['status']  = array();
-$lang->block->default['oa']['2']['params']['type']    = 'assignedTo';
+$lang->block->default['oa']['2']['params']['num'] = 15;
 
-$lang->block->default['oa']['3']['title'] = 'Project list';
-$lang->block->default['oa']['3']['block'] = 'project';
+$lang->block->default['oa']['3']['title'] = 'The task of assigned to me';
+$lang->block->default['oa']['3']['block'] = 'task';
 $lang->block->default['oa']['3']['grid']  = 4;
 
 $lang->block->default['oa']['3']['params']['num']     = 15;
 $lang->block->default['oa']['3']['params']['orderBy'] = 'id_desc';
-$lang->block->default['oa']['3']['params']['status']  = 'doing';
+$lang->block->default['oa']['3']['params']['status']  = array();
+$lang->block->default['oa']['3']['params']['type']    = 'assignedTo';
+
+$lang->block->default['oa']['4']['title'] = 'Project list';
+$lang->block->default['oa']['4']['block'] = 'project';
+$lang->block->default['oa']['4']['grid']  = 4;
+
+$lang->block->default['oa']['4']['params']['num']     = 15;
+$lang->block->default['oa']['4']['params']['orderBy'] = 'id_desc';
+$lang->block->default['oa']['4']['params']['status']  = 'doing';
 
 $lang->block->default['crm']['1']['title'] = 'My Order';
 $lang->block->default['crm']['1']['block'] = 'order';
@@ -121,13 +125,15 @@ $lang->block->default['team']['3']['params']['type'] = 'stick';
 
 $lang->block->default['sys']['1'] = $lang->block->default['oa']['1'];
 $lang->block->default['sys']['1']['source'] = 'oa';
-$lang->block->default['sys']['2'] = $lang->block->default['crm']['2'];
-$lang->block->default['sys']['2']['source'] = 'crm';
-$lang->block->default['sys']['3'] = $lang->block->default['crm']['1'];
+$lang->block->default['sys']['2'] = $lang->block->default['oa']['2'];
+$lang->block->default['sys']['2']['source'] = 'oa';
+$lang->block->default['sys']['3'] = $lang->block->default['crm']['2'];
 $lang->block->default['sys']['3']['source'] = 'crm';
-$lang->block->default['sys']['4'] = $lang->block->default['cash']['1'];
-$lang->block->default['sys']['4']['source'] = 'cash';
-$lang->block->default['sys']['5'] = $lang->block->default['team']['1'];
-$lang->block->default['sys']['5']['source'] = 'team';
-$lang->block->default['sys']['6'] = $lang->block->default['team']['2'];
+$lang->block->default['sys']['4'] = $lang->block->default['crm']['1'];
+$lang->block->default['sys']['4']['source'] = 'crm';
+$lang->block->default['sys']['5'] = $lang->block->default['cash']['1'];
+$lang->block->default['sys']['5']['source'] = 'cash';
+$lang->block->default['sys']['6'] = $lang->block->default['team']['1'];
 $lang->block->default['sys']['6']['source'] = 'team';
+$lang->block->default['sys']['7'] = $lang->block->default['team']['2'];
+$lang->block->default['sys']['7']['source'] = 'team';
