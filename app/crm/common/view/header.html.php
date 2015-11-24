5b46a39b06d1b862b8d3c4c9d0692a82a96513d2
chujilu <chujilu@cnezsoft.com>
2015-11-24 08:55:31 +0000

    * Fix a bug.
    git-svn-id: file:///tmp/ranzhi/trunk@3223 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/common/view/header.html.php
+++ app/crm/common/view/header.html.php
@@ -13,4 +13,4 @@
 <?php
 if($extView = $this->getExtViewFile(__FILE__)){include $extView; return helper::cd();}
 include $this->app->getBasePath() . 'app/sys/common/view/header.html.php';
-css::import($this->app->getClientTheme() . 'theme.crm.css');
+if(strpos($this->app->getClientTheme(), 'default') === false) css::import($this->app->getClientTheme() . 'theme.crm.css');
