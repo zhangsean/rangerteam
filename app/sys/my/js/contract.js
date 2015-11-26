2dafbe7da5a3aab164a52319853522faed8dca5c
chujilu <chujilu@cnezsoft.com>
2015-11-26 03:06:46 +0000

    * fix a bug.
    git-svn-id: file:///tmp/ranzhi/trunk@3235 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/my/js/contract.js
+++ app/sys/my/js/contract.js
@@ -1,5 +1,15 @@
 $(document).ready(function()
 {
+    /* Highlight submenu. */
+    if(config.requestType == 'GET')
+    {
+        $('#menu li').removeClass('active').find("[href*='=" + v.type + "']").parent().addClass('active');
+    }
+    else
+    {
+        $('#menu li').removeClass('active').find("[href*='-" + v.type + "']").parent().addClass('active');
+    }
+
     /* process actions. */
     $('.actions a').click(function()
     {
