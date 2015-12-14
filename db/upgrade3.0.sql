f7aee943f8ba74ddc9000102c7461df7f9644270
daitingting <daitingting@cnezsoft.com>
2015-12-14 06:18:10 +0000

    * Add originID in crm_contact table.
    git-svn-id: file:///tmp/ranzhi/trunk@3339 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/upgrade3.0.sql
+++ db/upgrade3.0.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `crm_contact`
 add `origin` varchar(150) NOT NULL AFTER `resume`,
+add `originID` mediumint(8) NOT NULL AFTER `origin`,
 add `status` enum('normal','wait','ignore') NOT NULL DEFAULT 'wait' AFTER `origin`,
 add `company` varchar(255) NOT NULL AFTER `phone`;
