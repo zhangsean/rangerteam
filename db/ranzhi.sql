1c7478c8a8b88b56122d85e592dddb05f6b44925
daitingting <daitingting@cnezsoft.com>
2015-12-22 02:55:10 +0000

    * Alter status in crm_customer.
    git-svn-id: file:///tmp/ranzhi/trunk@3413 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/ranzhi.sql
+++ db/ranzhi.sql
@@ -128,7 +128,7 @@ CREATE TABLE `crm_customer` (
   `size` tinyint(3) unsigned NOT NULL,
   `industry` mediumint(8) unsigned NOT NULL,
   `area` mediumint(8) unsigned NOT NULL,
-  `status` enum('potential', 'intension', 'signed', 'payed', 'failed') NOT NULL default 'potential',
+  `status` char(30) NOT NULL,
   `level` char(10) NOT NULL,
   `intension` text NOT NULL,
   `site` varchar(100) NOT NULL,
--- db/upgrade3.0.sql
+++ db/upgrade3.0.sql
@@ -3,3 +3,5 @@ add `origin` varchar(150) NOT NULL AFTER `resume`,
 add `originID` mediumint(8) NOT NULL AFTER `origin`,
 add `status` enum('normal','wait','ignore') NOT NULL DEFAULT 'wait' AFTER `origin`,
 add `company` varchar(255) NOT NULL AFTER `phone`;
+
+ALTER TABLE `crm_customer` CHANGE `status` `status` char(30) NOT NULL;
