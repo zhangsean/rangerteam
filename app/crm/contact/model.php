961d71f0c3614f5e4304e4bc3e6c5ff73418217c
daitingting <daitingting@cnezsoft.com>
2015-12-25 07:35:51 +0000

    * Origin may be more than one.
    git-svn-id: file:///tmp/ranzhi/trunk@3433 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/contact/model.php
+++ app/crm/contact/model.php
@@ -135,7 +135,7 @@ class contactModel extends model
             ->leftJoin(TABLE_RESUME)->alias('t2')->on('t1.resume = t2.id')
             ->where('t1.deleted')->eq(0)
             ->andWhere('status')->eq($status)
-            ->beginIF($origin)->andWhere('origin')->eq($origin)->fi()
+            ->beginIF($origin)->andWhere('origin')->like("%$origin%")->fi()
             ->beginIF($status == 'normal')->andWhere('t2.customer')->in($customerIdList)->fi()
             ->beginIF($customer)->andWhere('t1.id')->in(array_keys($resumes))->fi()
             ->beginIF($mode == 'past')->andWhere('t1.nextDate')->lt(helper::today())->andWhere('t1.nextDate')->ne('0000-00-00')->fi()
