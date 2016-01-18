2f2b30681927d8041cc7ddf2205b0cdd647b6c47
liugang <liugang@cnezsoft.com>
2016-01-18 07:09:15 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3521 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/common/lang/zh-cn.php
+++ app/crm/common/lang/zh-cn.php
@@ -68,7 +68,7 @@ $lang->contact->menu->thismonth = '本月内联系|contact|browse|mode=thismonth
 
 $lang->leads = new stdclass();
 $lang->leads->menu = new stdclass();
-$lang->leads->menu->assignedTo  = '指派给我|leads|browse|mode=assignedTo';
+$lang->leads->menu->assignedTo  = array('link' => '指派给我|leads|browse|mode=assignedTo', 'alias' => 'create');
 $lang->leads->menu->next        = '下次联系|leads|browse|mode=next';
 $lang->leads->menu->ignoredBy   = '由我忽略|leads|browse|mode=ignoredBy&status=ignore';
 $lang->leads->menu->public      = '公共名单|leads|browse|mode=all&status=ignore';
