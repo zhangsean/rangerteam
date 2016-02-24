0a49bb0eaeeb61dd626682e8d47ccfa68bb7003c
liugang <liugang@cnezsoft.com>
2016-02-24 07:13:25 +0000

    * Fixed a bug that the schema can't be imported.
    git-svn-id: file:///tmp/ranzhi/trunk@3645 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/cash/trade/model.php
+++ app/cash/trade/model.php
@@ -482,7 +482,7 @@ class tradeModel extends model
             $trade->createCustomer = isset($this->post->createCustomer[$key]) ? $this->post->createCustomer[$key] : '';
             $trade->customerName   = isset($this->post->customerName[$key])   ? $this->post->customerName[$key] : '';
             $trade->handlers       = !empty($this->post->handlers[$key]) ? join(',', $this->post->handlers[$key]) : '';
-            $trade->product        = $this->post->product[$key];
+            $trade->product        = isset($this->post->product[$key])        ? $this->post->product[$key] : 0;
             $trade->date           = $this->post->date[$key];
             $trade->desc           = strip_tags(nl2br($this->post->desc[$key]));
             $trade->currency       = $depositor->currency;
