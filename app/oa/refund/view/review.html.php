4220c5fbe122d2171797f8c6f6e397640c078679
daitingting <daitingting@cnezsoft.com>
2016-04-07 06:27:24 +0000

    * Adjust style for review of refund.
    git-svn-id: file:///tmp/ranzhi/trunk@3699 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/refund/view/review.html.php
+++ app/oa/refund/view/review.html.php
@@ -15,15 +15,16 @@
 <?php include '../../../sys/common/view/chosen.html.php';?>
 <?php js::set('detail', !empty($refund->detail) ? true : false);?>
 <form method='post' id='ajaxForm' action='<?php echo inlink('review', "refundID={$refund->id}")?>'>
-  <?php if(!empty($refund->detail)):?>
-  <table class='table table-form'>
+  <table class='table table-bordered'>
+    <thead>
     <tr class='text-center'>
       <th class='w-80px'><?php echo $lang->refund->date;?></th>
-      <th class='w-100px text-right'><?php echo $lang->refund->money;?></th>
+      <th class='w-100px'><?php echo $lang->refund->money;?></th>
       <th class='w-100px'><?php echo $lang->refund->status;?></th>
       <th class='w-120px'><?php echo $lang->refund->category;?></th>
       <th><?php echo $lang->refund->desc;?></th>
-      <th>
+      <?php if(!empty($refund->detail)):?>
+      <th class='w-160px'>
         <div class='btn-group'>
           <button class='btn btn-mini all-pass' type='button'><?php echo $lang->refund->reviewAllStatusList['allpass'];?></button>
           <button class='btn btn-mini all-reject' type='button'><?php echo $lang->refund->reviewAllStatusList['allreject'];?></button>
@@ -31,7 +32,12 @@
           <?php echo html::input('allReject', '0', "class='hide'");?>
         </div>
       </th>
+      <?php else:;?>
+      <th></th>
+      <?php endif;?>
     </tr>
+    </thead>
+    <?php if(!empty($refund->detail)):?>
     <?php foreach($refund->detail as $detail):?>
     <tr class='text-center'>
       <td><?php echo $detail->date;?></td>
@@ -42,30 +48,32 @@
       <td><?php echo html::radio("status{$detail->id}", $lang->refund->reviewStatusList, $detail->status == 'reject' ? 'reject' : 'pass');?></td>
     </tr>
     <?php endforeach;?>
-  </table>
-  <?php endif;?>
-  <table class='table table-form'>
-    <?php if(empty($refund->detail)):?>
-    <tr>
-      <th class='w-60px'><?php echo $lang->refund->status;?></th>
-      <td class='text-left'><?php echo html::radio("status", $lang->refund->reviewStatusList, 'pass');?></td>
+    <?php else:?>
+    <tr class='text-center'>
+      <td><?php echo $refund->date;?></td>
+      <td class='text-right'><?php echo zget($currencySign, $refund->currency) . "<span class='detailMoney'>" . $refund->money . "</span>";?></td>
+      <td><?php echo $lang->refund->statusList[$refund->status];?></td>
+      <td><?php echo $categories[$refund->category];?></td>
+      <td><?php echo $refund->desc?></td>
+      <td><?php echo html::radio("status", $lang->refund->reviewStatusList, $refund->status == 'reject' ? 'reject' : 'pass');?></td>
     </tr>
     <?php endif;?>
+  </table>
+  <table class='table table-form'>
     <tr class='reviewMoney'>
-      <th class='w-60px'><?php echo $lang->refund->reviewMoney;?></th>
+      <th class='w-80px'><?php echo $lang->refund->reviewMoney;?></th>
       <td>
         <div class='input-group'>
           <?php echo html::input('money', $refund->money, "class='form-control'");?>
           <span class='input-group-addon'><?php echo zget($lang->currencyList, $refund->currency, $refund->currency);?></span>
         </div>
       </td>
+      <td><?php echo html::submitButton();?></td>
     </tr>
     <tr class='reason hide'>
       <th class='w-60px'><?php echo $lang->refund->reason;?></th>
       <td><?php echo html::textarea("reason", '', "class='form-control rowspan=3'");?></td>
-    </tr>
-    <tr>
-      <th></th><td><?php echo html::submitButton();?></td>
+      <td><?php echo html::submitButton();?></td>
     </tr>
   </table>
 </form>
