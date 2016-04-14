1cba8c60c6fbe4e5b558428fc80c270600ef6251
daitingting <daitingting@cnezsoft.com>
2016-04-14 03:12:15 +0000

    * Adjust codes to compatible IE.
    git-svn-id: file:///tmp/ranzhi/trunk@3744 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/leave/js/edit.js
+++ app/oa/leave/js/edit.js
@@ -8,8 +8,8 @@ $(document).ready(function()
         var finish = $('#finish').val();
         if(!begin || !end || !start || !finish) return false;
 
-        begin = begin.replace(/-/g, ',');
-        end   = end.replace(/-/g, ',');
+        begin = begin.replace(/-/g, '/');
+        end   = end.replace(/-/g, '/');
 
         var hours = 0;
         var beginTime = Date.parse(new Date(begin + ' ' + start));
