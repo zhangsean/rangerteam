ab72171080d5935e668e95e42e3ce9794b170153
liugang <liugang@cnezsoft.com>
2016-04-18 01:33:09 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3772 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/upgrade3.2.1.sql
+++ db/upgrade3.2.1.sql
@@ -1,5 +1,3 @@
-UPDATE `oa_todo` set `type` = 'custom' where `type` = 'undone';
-ALTER TABLE `sys_product` ADD `code` varchar(20) NOT NULL AFTER `name`;
 ALTER TABLE `crm_customer` CHANGE `id`       `id`       mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
 ALTER TABLE `crm_service`  CHANGE `customer` `customer` mediumint(8) unsigned NOT NULL;
 ALTER TABLE `oa_doc`       CHANGE `views`    `views`    mediumint(8) unsigned NOT NULL;
@@ -12,6 +10,9 @@ ALTER TABLE `sys_entry`    CHANGE `id`       `id`       mediumint(8) unsigned NO
 ALTER TABLE `sys_tag`      CHANGE `id`       `id`       mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
 ALTER TABLE `sys_user`     CHANGE `id`       `id`       mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
 
+ALTER TABLE `sys_product`  ADD `code` varchar(20) NOT NULL AFTER `name`;
 ALTER TABLE `sys_category` ADD `major` enum('0','1') NOT NULL DEFAULT '0';
-ALTER TABLE `sys_entry` ADD `category` mediumint(8) NOT NULL DEFAULT '0';
+ALTER TABLE `sys_entry`    ADD `category` mediumint(8) NOT NULL DEFAULT '0';
 ALTER TABLE `sys_user` CHANGE `role` `role` char(30) NOT NULL;
+
+UPDATE `oa_todo` set `type` = 'custom' where `type` = 'undone';
