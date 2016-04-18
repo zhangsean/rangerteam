c3b1a622e3748a899801c459db5f04b203e1663e
liugang <liugang@cnezsoft.com>
2016-04-18 08:41:12 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3777 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/upgrade/view/execute.html.php
+++ app/sys/upgrade/view/execute.html.php
@@ -14,7 +14,7 @@
   <div class='modal-dialog w-450px'>
     <?php if($result == 'fail'):?>
     <div class='modal-header'><h3><?php echo $lang->upgrade->fail;?></h3></div>
-    <div class='modal-body'><?php echo nl2br(join('\n', $errors)); ?></div>
+    <div class='modal-body'><?php echo join('<br />', $errors); ?></div>
     <?php else:?>
     <div class='modal-body'><div class='alert alert-success text-center'><h4><?php echo $lang->upgrade->success;?></h4></div></div>
     <div class='modal-footer'><?php echo html::a('index.php', $lang->home, "class='btn btn-success'");?></div>
