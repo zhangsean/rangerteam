48789fde71cbf5aab4e20eb63d7803e01736a822
sunhao <sunhao@cnezsoft.com>
2016-07-04 09:35:27 +0000

    - remove unused code.
    git-svn-id: file:///tmp/ranzhi/trunk@3921 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/sys/common/model.php
+++ app/sys/common/model.php
@@ -1367,44 +1367,4 @@ class commonModel extends model
         fwrite($fh, "\n");
         fclose($fh);
     }
-
-    /**
-     * Print user avatar
-     * 
-     * @param  string $size
-     * @static
-     * @access public
-     * @return void
-     */
-    public static function printUserAvatar($class = 'circle', $user = null)
-    {
-        global $app;
-        if($user === null) $user = $app->user;
-
-        echo "<div class='avatar text-tint $class' data-skin='{$user->id}'>";
-        if(!empty($user->avatar)) echo html::image($user->avatar);
-        else echo (!empty($user->realname) ? $user->realname : (!empty($user->name) ? $user->name : (!empty($user->account) ? $user->account : '<i class="icon icon-user"></i>')));
-        echo '</div>';
-    }
-
-    /**
-     * Format bytes
-     *
-     * @param  number  $bytes
-     * @param  integer $precision
-     * @static
-     * @access public
-     * @return string
-     */
-    public static function formatBytes($bytes, $precision = 2)
-    { 
-        $units = array('B', 'KB', 'MB', 'GB', 'TB'); 
-
-        $bytes  = max($bytes, 0); 
-        $pow    = floor(($bytes ? log($bytes) : 0) / log(1024)); 
-        $pow    = min($pow, count($units) - 1);
-        $bytes /= (1 << (10 * $pow)); 
-
-        return round($bytes, $precision) . ' ' . $units[$pow]; 
-    }
 }
