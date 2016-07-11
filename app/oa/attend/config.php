9c30926d64080afc8caf0263b212667b49ccd228
liugang <liugang@cnezsoft.com>
2016-07-11 09:43:42 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3939 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/oa/attend/config.php
+++ app/oa/attend/config.php
@@ -1,8 +1,9 @@
 <?php
 if(!isset($comfig->attend)) $config->attend = new stdclass();
-$config->attend->signInLimit  = '9:00';
-$config->attend->signOutLimit = '18:00';
-$config->attend->workingHours = '8';
-$config->attend->workingDays  = '5';
-$config->attend->mustSignOut  = 'yes';
-$config->attend->ip           = '*';
+$config->attend->signInLimit   = '9:00';
+$config->attend->signOutLimit  = '18:00';
+$config->attend->workingHours  = '8';
+$config->attend->workingDays   = '5';
+$config->attend->mustSignOut   = 'yes';
+$config->attend->ip            = '*';
+$config->attend->noAttendUsers = '';
