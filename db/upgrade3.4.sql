3f6bd745fbb3652d41128b0cf203185793e98229
liugang <liugang@cnezsoft.com>
2016-07-19 02:06:26 +0000

    * Add default rights of trade compare.
    git-svn-id: file:///tmp/ranzhi/trunk@3981 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- db/upgrade3.4.sql
+++ db/upgrade3.4.sql
@@ -2,12 +2,13 @@ ALTER TABLE oa_refund CHANGE `status` `status` enum('draft','wait','doing','pass
 ALTER TABLE cash_trade CHANGE `type` `type` enum('in','out','transferin','transferout','invest','redeem') NOT NULL;
 ALTER TABLE sys_category CHANGE `major` `major` enum('0','1','2','3','4') NOT NULL DEFAULT '0';
 
-
 INSERT INTO `sys_grouppriv` (`group`, `module`, `method`) VALUES
+(1,'trade','compare'),
 (1,'trade','export2Excel'),
 (1,'attend','detail'),
 (1,'attend','exportDetail'),
 (1,'leave','export'),
 (1,'overtime','export'),
 (1,'refund','export'),
+(2,'trade','compare'),
 (2,'trade','export2Excel');
