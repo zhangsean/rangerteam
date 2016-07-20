91af2b838c946fb002427ed22b96a1ffe6dd0a99
daitingting <daitingting@cnezsoft.com>
2016-07-20 07:11:44 +0000

    * Adjust codes.
    git-svn-id: file:///tmp/ranzhi/trunk@3999 cb094fd3-5a31-0410-bd9d-ef5a9cbf4261

--- app/crm/leads/config.php
+++ app/crm/leads/config.php
@@ -17,7 +17,7 @@ $config->leads->editor->view   = array('id' => 'comment,lastComment', 'tools' =>
 
 global $lang, $app;
 $app->loadLang('contact', 'crm');
-$config->leads->search['module'] = 'contact';
+$config->leads->search['module'] = 'leads';
 
 $config->leads->search['fields']['realname']      = $lang->contact->realname;
 $config->leads->search['fields']['origin']        = $lang->contact->origin;
