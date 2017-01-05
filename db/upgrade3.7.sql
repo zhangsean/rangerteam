3a8c84ee4c9671a50bf7a00f579a6ce5a8b398fe
daitingting <daitingting@xirangit.com>
2017-01-05 17:21:26 +0800

    * Adjust app for block of project and task.

--- db/upgrade3.7.sql
+++ db/upgrade3.7.sql
@@ -15,3 +15,5 @@ DELETE FROM `sys_grouppriv` WHERE `module`='leave' AND `method`='reviewBack';
 
 INSERT INTO `sys_entry` (`name`, `abbr`, `code`, `buildin`, `integration`, `open`, `key`, `ip`, `logo`, `login`, `control`, `size`, `position`, `visible`, `order`) VALUES
 ('文档', '文档', 'doc', 1, 1, 'iframe', '76ff605479df34f1d239730efa68d562', '*', 'theme/default/images/ips/app-doc.png', '../doc', 'simple', 'max', 'default', 1, 26);
+
+UPDATE `sys_block` SET `app`='proj',`source`='proj' WHERE `app`='oa' AND `source`='oa' AND `block` in ('project','task');
