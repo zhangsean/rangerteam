760f402620bd2b5bfb67d14f3e1703abce677013
tengfei <tengfei@cnezsoft.com>
2017-01-24 13:40:12 +0800

    *adjust error in sys/commom

--- app/sys/common/lang/en.php
+++ app/sys/common/lang/en.php
@@ -39,11 +39,7 @@ $lang->apps->superadmin = 'ADMIN';
 
 /* Lang items for ranzhi. */
 $lang->ranzhi    = 'Ranger';
-<<<<<<< HEAD
-$lang->agreement = "I Agree to the <a href='http://zpl.pub/page/zplv11.html' target='_blank'>Z PUBLIC LICENSE 1.2</a>, <span class='text-danger'>and will keep the logo and link of Ranger.</span>";
-=======
 $lang->agreement = "I have read and agreed to  <a href='http://zpl.pub/page/zplv11.html' target='_blank'>Z PUBLIC LICENSE 1.2</a>, <span class='text-danger'>and will keep the logos and links of Ranger.</span>";
->>>>>>> d5922087d0bc082bff9888ba87a7108ff6b9be09
 $lang->poweredBy = "<a href='http://www.ranzhico.com/?v=%s' target='_blank'>{$lang->ranzhi} %s</a>";
 $lang->ipLimited = "<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8' /></head><body>Sorry, your current IP is blocked. Please contact the Administrator to get privilege.</body></html>";
 
